f3ef8e7ece86cd98bfefedb9d969cc9c355fda36
Willian Prado <willian_prado@hotmail.com>
2021-07-30 16:24:20 -0300

    Req 1 - Crie uma página inicial de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,42 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { user, password } = this.state;
+    return (
+      <section>
+        <div>Login</div>
+        <input
+          type="text"
+          value={ user }
+          data-testid="email-input"
+          onChange={ this.handleChange }
+        />
+        <input
+          type="password"
+          value={ password }
+          data-testid="password-input"
+          onChange={ this.handleChange }
+        />
+        <button type="button"> Entrar </button>
+      </section>
+    );
   }
 }
 
