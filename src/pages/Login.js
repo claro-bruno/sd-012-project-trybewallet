fc40af67744fa1369ef6c1d307a62fe21568402f
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-07-30 15:06:43 -0300

    Criada a pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        Trybewallet
+        <input type="email" placeholder="Email" data-testid="email-input" />
+        <input type="password" placeholder="Senha" data-testid="password-input" />
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
