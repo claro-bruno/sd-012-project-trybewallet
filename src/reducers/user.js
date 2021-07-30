f1940b5cbfe88d8a7009af342820cf9d47bf91ab
Josimar Souza <contact.josimarsouza@gmail.com>
2021-07-30 15:12:39 -0300

    Criado reducers iniciais

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,8 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = {
+  email: '',
+};
+
+const user = (state = initialState /* action */) => state;
+
+export default user;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,9 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const initialState = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = initialState /* action */) => state;
+
+export default wallet;
