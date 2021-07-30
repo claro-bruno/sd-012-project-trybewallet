2f81536f5f2dd3a3c8c9e453e4c2f0f6da69cade
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-30 15:56:40 -0300

    cria e configura os inputs email e password da page Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,59 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange({ target: { name, type, value, checked } }) {
+    function newValue() {
+      switch (type) {
+      case 'checkbox': return checked;
+      case 'number': return +value;
+      default: return value;
+      }
+    }
+    this.setState((state) => ({ ...state, [name]: newValue() }));
+  }
+
   render() {
-    return <div>Login</div>;
+    const { state: { email, password }, handleChange } = this;
+    return (
+      <section>
+        <h1>Login</h1>
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              id="email"
+              name="email"
+              data-testid="email-input"
+              value={ email }
+              onChange={ handleChange }
+            />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input
+              type="password"
+              id="password"
+              name="password"
+              data-testid="password-input"
+              value={ password }
+              onChange={ handleChange }
+            />
+          </label>
+        </form>
+      </section>
+    );
   }
 }
 
