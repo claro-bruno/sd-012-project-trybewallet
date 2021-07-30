4247ea3f8de11bd97ec281b1f3390c369004021a
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-30 18:18:48 -0400

    cria o formulario de nova despesa

---
+++ src/components/ExpenseForm/index.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import fetchApi from '../../actions';
+
+class ExpenseForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      id: 0,
+      value: '',
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
+      description: '',
+    };
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClick() {
+    const { createNewExpense, expenses } = this.props;
+    const expensesLength = expenses.length;
+
+    createNewExpense(this.state);
+
+    if (expensesLength >= 0) {
+      this.setState({ id: expensesLength + 1 });
+    }
+  }
+
+  render() {
+    const { currencies } = this.props;
+    return (
+      <form>
+        <label htmlFor="val">
+          Valor
+          <input type="text" id="val" name="value" onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="des">
+          Descrição
+          <input type="text" id="des" name="description" onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="cur">
+          Moeda
+          <select id="cur" name="currency" onChange={ this.handleChange }>
+            {currencies.map((currency) => (
+              <option key={ currency } value={ currency }>{ currency }</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="payment">
+          Método de pagamento
+          <select id="payment" name="method" onChange={ this.handleChange }>
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          Tag
+          <select id="tag" name="tag" onChange={ this.handleChange }>
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </label>
+        <button
+          type="button"
+          onClick={ this.handleClick }
+        >
+          Adicionar despesa
+        </button>
+      </form>
+    );
+  }
+}
+
+ExpenseForm.propTypes = {
+  newExpense: PropTypes.func,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  createNewExpense: (expense) => dispatch(fetchApi(expense)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseForm);
