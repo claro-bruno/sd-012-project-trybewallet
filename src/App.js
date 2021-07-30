a4e22add0a96a6dae17f4a8844f7057a48abc669
Wellington <junior.wel02@exemplo.br>
2021-07-30 15:09:38 -0300

    Correçao lint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import Login from './pages/Login'
+import Login from './pages/Login';
 
 function App() {
   return (
     <Switch>
-      <Route path="/" component={ Login }  />
+      <Route path="/" component={ Login } />
     </Switch>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,12 +3,18 @@ import React from 'react';
 class Login extends React.Component {
   render() {
     return (
-      <form>
-        <input data-testid="email-input" type="text" placeholder="Seu Usario aqui" />
-        <input data-testid="password-input" type="password" placeholder="Sua senha aqui" />
-        <button type="submit">Entrar</button>
-      </form>
-    ); 
+      <div>
+        <form>
+          <input data-testid="email-input" type="text" placeholder="Seu Usario aqui" />
+          <input
+            data-testid="password-input"
+            type="password"
+            placeholder="Sua senha aqui"
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
