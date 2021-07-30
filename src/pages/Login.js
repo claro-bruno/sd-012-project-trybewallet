1a9c5d80531c293f65a8271cf31ed7ba53a51d30
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-07-30 15:33:33 -0300

    Criado a pagina de login apenas com campo email, password e botao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,55 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <label htmlFor="email-input">
+          E-mail
+          <input
+            name="email"
+            value={ email }
+            data-testid="email-input"
+            type="email"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password-input">
+          Password
+          <input
+            name="password"
+            value={ password }
+            data-testid="password-input"
+            type="password"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button type="submit">Entrar</button>
+      </form>
+    );
   }
 }
 
