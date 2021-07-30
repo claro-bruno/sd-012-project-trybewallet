0f57a699011f973d7bc05af7c1c17eb1e3c363a5
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-07-30 16:40:57 -0300

    Comecando projeto, adiciona req 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <div>Login</div>
+        <label htmlFor="emailInput">
+          <input
+            id="emailInput"
+            data-testid="email-input"
+            type="email"
+            placeholder="E-mail"
+          />
+        </label>
+        <label htmlFor="passwordInput">
+          <input
+            id="passwordInput"
+            data-testid="password-input"
+            type="password"
+            placeholder="Password"
+          />
+        </label>
+        <button id="loginButton" type="submit">
+          Login
+        </button>
+      </div>
+    );
   }
 }
 
