664b587f63544450d2cb3924ef3dae2466fa6209
Wellington <junior.wel02@exemplo.br>
2021-07-30 16:02:24 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import Login from './pages/Login';
+import Wallet from './reducers/wallet';
 
 function App() {
   return (
     <Switch>
+      <Route path="/carteira" component={ Wallet } />
       <Route path="/" component={ Login } />
     </Switch>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,64 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      valid: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validation = this.validation.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => this.validation());
+  }
+
+  /* Referencia da validação com regextest usada: https://cursos.alura.com.br/forum/topico-como-validar-email-e-senha-em-javascript-80469  */
+  validation() {
+    const { email, password } = this.state;
+    const minLength = 5;
+    const Patt = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+    const valid = !(Patt.test(email) && password.length > minLength);
+    this.setState({ valid });
+  }
+
   render() {
+    const { email, password, valid } = this.state;
+
     return (
       <div>
         <form>
-          <input data-testid="email-input" type="text" placeholder="Seu Usario aqui" />
+          <input
+            data-testid="email-input"
+            type="email"
+            placeholder="Seu Email aqui"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+
           <input
             data-testid="password-input"
             type="password"
             placeholder="Sua senha aqui"
+            name="password"
+            minLength="6"
+            value={ password }
+            onChange={ this.handleChange }
           />
-          <button type="submit">Entrar</button>
+
+          <button
+            type="submit"
+            id="butaoSubmit"
+            disabled={ valid }
+          >
+            Entrar
+          </button>
         </form>
       </div>
     );
