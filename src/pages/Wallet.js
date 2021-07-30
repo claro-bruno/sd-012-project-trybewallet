58ac32410c6c4af95940e955cc4cd51510bb81e2
Renata Nunes <renatanunes231@yahoo.com.br>
2021-07-30 20:35:21 -0300

    Header da pagina da carteira criada

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+
+    return (
+      <header>
+        <p data-testid="email-field">{ email }</p>
+        <p data-testid="total-field">{ `R$${0}`}</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToPrps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToPrps)(Wallet);
