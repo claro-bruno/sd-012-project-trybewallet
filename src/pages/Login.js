cd5c2016477fadf5dc3942979aa2cb1857eed21b
Kevin Oliveira <kevin.zero@hotmail.com>
2021-07-30 15:50:27 -0300

    2 - Acrescenta verificacao de dados

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,74 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      blockSubmit: true,
+      redirect: false,
+    };
+
+    this.validateInputs = this.validateInputs.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  validateInputs() {
+    const { email, password } = this.state;
+    const validEmail = /\S+@\S+\.\S+/;
+    const validPassword = 6;
+    if (validEmail.test(email) && password.length >= validPassword) {
+      this.setState({ blockSubmit: false });
+    } else {
+      this.setState({ blockSubmit: true });
+    }
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value }, () => this.validateInputs());
+  }
+
+  handleClick() {
+    this.setState({ redirect: true });
+  }
+
   render() {
+    const { email, password, blockSubmit, redirect } = this.state;
+
+    if (redirect) {
+      return <Redirect to="/carteira" />;
+    }
+
     return (
       <main>
         <form>
-          <input type="text" placeholder="EMAIL" data-testid="email-input" />
-          <input type="password" placeholder="SENHA" data-testid="password-input" />
-          <Link to="/carteira">Entrar</Link>
+          <input
+            name="email"
+            type="text"
+            placeholder="EMAIL"
+            data-testid="email-input"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="SENHA"
+            data-testid="password-input"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            disabled={ blockSubmit }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
         </form>
       </main>
     );
