a9f6420dcbd01a4f62fb3c932415801276c3dc88
Tulio <tuliotrindade99@gmail.com>
2021-07-30 18:11:35 -0300

    requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,11 +6,47 @@ class Wallet extends React.Component {
   render() {
     const { email } = this.props;
     return (
-      <header>
-        <p data-testid="email-field" id="email">{ email }</p>
-        <p data-testid="total-field"> 0 </p>
-        <p data-testid="header-currency-field">BRL</p>
-      </header>
+      <div>
+        <header>
+          <p data-testid="email-field" id="email">{ email }</p>
+          <p data-testid="total-field"> 0 </p>
+          <p data-testid="header-currency-field">BRL</p>
+        </header>
+        <form>
+          <label htmlFor="valor">
+            valor
+            <input id="valor" />
+          </label>
+          <label htmlFor="descrição">
+            descrição
+            <textarea type="text" id="descrição" name="descrição" />
+          </label>
+          <label htmlFor="moeda">
+            moeda
+            <select id="moeda">
+              { null }
+            </select>
+          </label>
+          <label htmlFor="metodopagamento">
+            Método de pagamento
+            <select id="metodopagamento">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            tag
+            <select id="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
+      </div>
     );
   }
 }
