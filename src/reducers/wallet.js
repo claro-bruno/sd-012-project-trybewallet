b8b6989c5a7ad313a448883c9f63f634a7cf4864
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-30 15:32:01 -0300

    configura reducer wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,13 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+function wallet(state = INITIAL_STATE, action) {
+  const { type } = action;
+  switch (type) {
+  default: return state;
+  }
+}
+
+export default wallet;
