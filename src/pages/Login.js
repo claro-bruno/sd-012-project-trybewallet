22b549d25b9137e4db269553af50a41901682d95
Eurides <euridesalves@yahoo.com.br>
2021-07-30 18:59:57 -0300

    Adicionado requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,60 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  checkLogin() {
+    const { email, password } = this.state;
+    const emailRegex = email.match(/^[a-z0-9_.-]+@[a-z]+\.[a-z]{2,3}(?:\.[a-z]{2})?$/);
+    const lenPassword = 6;
+    if (password.length >= lenPassword && emailRegex) {
+      console.log('condicao correta');
+
+      return false;
+    }
+    console.log('com erro');
+    return true;
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="inputEmail" data-testid="email-input">
+          E-mail
+          <input
+            type="email"
+            name="email"
+            // value={ textLogin }
+            placeholder="E-mail"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="inputPassword" data-testid="password-input">
+          Senha
+          <input
+            type="password"
+            name="password"
+            // value={ passwordLogin }
+            placeholder="Senha"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button type="button" disabled={ this.checkLogin() }>Entrar</button>
+      </div>);
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,14 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const STATE_INITIAL = {
+  login: {},
+  senha: {},
+};
+
+function reducerLogin(state = STATE_INITIAL, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default reducerLogin;
