569b05733cdc7eb73b6a65f4c10949fe38403844
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-30 18:19:38 -0400

    cria a pagina da carteira

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import HeaderComponent from '../components/HeaderComponents';
+import ExpenseForm from '../components/ExpenseForm';
+import ExpenseTable from '../components/ExpenseTable';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currencies: [],
+    };
+
+    this.currencies = this.currencies.bind(this);
+  }
+
+  componentDidMount() {
+    this.currencies();
+  }
+
+  async currencies() {
+    const URL = 'https://economia.awesomeapi.com.br/json/all';
+
+    const fetchApiResquest = await fetch(URL);
+    const jsonResponse = await fetchApiResquest.json();
+    const allCurrencies = Object.keys(jsonResponse);
+    this.setState({
+      currencies: [...allCurrencies],
+    });
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { currencies } = this.state;
+    return (
+      <div>
+        <HeaderComponent />
+        <ExpenseForm currencies={ currencies } />
+        <ExpenseTable />
+      </div>
+    );
   }
 }
 
