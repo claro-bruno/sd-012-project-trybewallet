920c2f8348439d26f327feb91dac5f17b2aaf61b
Teika <shimworks.tk@gmail.com>
2021-07-30 14:49:31 -0300

    inciando projeto

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,16 @@
-// import user from './user';
-// import wallet from './wallet';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import wallet from './wallet';
+import user from './user';
 
-// Configure os seus reducers.
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+const rootReducer = combineReducers({ user, wallet });
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
+
+export default store;
