5360379894418bc38ce3837f4c8ebf7752d99239
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-07-30 15:35:48 -0300

    Converte App.js a classe

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends Component {
+  render() {
+    return <div>Hello, TrybeWallet!</div>;
+  }
 }
 
 export default App;
