e6430aae462ae288becaa3cea49f0becba06181c
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-30 14:59:24 -0300

    criando componente Button

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Button extends React.Component {
+  render() {
+    const { text, name, onClick }
+    return (
+      <button name={name} onClick={ onClick }>{ text }</button>
+    );
+  }
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Button;
