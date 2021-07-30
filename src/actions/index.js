f01c5d80a9cf55be5ec7567fa54536b13ccd8ffc
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-07-30 17:30:39 -0300

    Feat: Setting the actions files

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 // Coloque aqui suas actions
+import { USER_ACTION, WALLET_ACTION } from './actionsTypes';
+
+export const user = (valor) => ({
+  type: USER_ACTION,
+  valor,
+});
+
+export const wallet = (valor) => ({
+  type: WALLET_ACTION,
+  valor,
+});
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,24 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case WALLET_ACTION:
+    return ({
+      ...state,
+      state,
+    });
+  default:
+    return state;
+  }
+};
+
+export default wallet;
