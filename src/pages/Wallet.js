d00d667a06413c62538d126fd53a5db8866ac495
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-07-30 16:51:04 -0300

    Requisito 4 e 5, renderizando a carteira no local correto, mostrando valores iniciais manualmente ainda nao configurados no redux

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <header>
+          <p>
+            E-mail:
+            <span data-testid="email-field">
+              { email }
+            </span>
+          </p>
+          <p>
+            Despesa Total: R$
+            <span data-testid="total-field"> 0 </span>
+            <span data-testid="header-currency-field">BRL</span>
+          </p>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
