24bc382aa8b42687cd05fd50e3dd349a31222823
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-30 13:43:51 -0400

    cria wallet reducer para nova despesa

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,24 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const walletReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'newExpense':
+    return {
+      ...state,
+      wallet: {
+        currencies: [...state.wallet.currencie, action.currencie],
+        expenses: [...state.wallet.expense, action.expense],
+      },
+    };
+
+  default:
+    return state;
+  }
+};
+
+export default walletReducer;
