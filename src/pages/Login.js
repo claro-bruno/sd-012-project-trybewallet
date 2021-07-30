1422d75912d286605830681b7a88f0cff8f67dc1
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-07-30 16:03:32 -0300

    Fix: ESlint issue

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 import LoginForm from '../components/LoginForm';
 
 class Login extends React.Component {
