a56d40d987f8861f476f9e693f0f8bd116711594
cristianocsl <cslcristiano@gmail.com>
2021-07-30 19:25:25 -0300

    Reguisito 4:  Crie uma página para sua carteira com rota /carteira

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-// import Wallet from '../pages/Wallet';
+import Wallet from './pages/Wallet';
 import Login from './pages/Login';
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={ Login } />
+      <Route path="/carteira" component={ Wallet } />
     </div>
   );
 }
