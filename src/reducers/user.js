5a4f9f1194e8403ee9992c06ff9ddb5c7089169d
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-07-30 16:57:10 -0300

    Feat: Setting up User Reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,24 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case EMAIL_ACTION:
+    return ({
+      ...state,
+      email: action.email,
+    });
+  default:
+    return state;
+  }
+};
+
+export default user;
