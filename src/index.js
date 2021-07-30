447d8012e9f28bbf837a44b85d676924dba654ee
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2021-07-30 16:19:46 -0300

    Requisito 1 - Pagina de Login

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
+import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={ store }>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,41 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailInputText: '',
+      passwordInputText: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { emailInputText, passwordInputText } = this.state;
+    return (
+      <div>
+        <form onSubmit={ this.submitLogin }>
+          <label htmlFor="email-input">
+            Email:
+            <input
+              type="text"
+              id="email-input"
+              data-testid="email-input"
+              value={ emailInputText }
+            />
+          </label>
+          <label htmlFor="password-input">
+            Senha:
+            <input
+              type="text"
+              id="password-input"
+              data-testid="password-input"
+              value={ passwordInputText }
+            />
+          </label>
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
