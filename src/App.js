9c2aa3d5b61a300bffd8e35854851da30e741668
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-07-30 16:42:51 -0300

    adiciona requisito 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div>
       Hello, TrybeWallet!
       <Switch>
-        <Route path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+        <Route exact path="/" component={ Login } />
       </Switch>
     </div>);
 }
