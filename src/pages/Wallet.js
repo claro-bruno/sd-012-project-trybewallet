b0bcef82b1a82f731192debc9db280156fb7b86f
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-07-30 18:12:31 -0300

    Requisito 5 - Cria Header contendo informaçoes do usuario

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      currency: 'BRL',
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { total, currency } = this.state;
+    const { userEmail } = this.props;
+    return (
+      <div>
+        <header>
+          <span data-testid="email-field">{`Email: ${userEmail}`}</span>
+          <span data-testid="total-field">{`Despesa Total: R$ ${total}`}</span>
+          <span data-testid="header-currency-field">{`Moeda: ${currency}`}</span>
+        </header>
+      </div>);
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
