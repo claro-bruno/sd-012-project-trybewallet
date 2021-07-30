a90415b244ff8577a355bcd9238f558b4390cd83
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-07-30 17:29:19 -0300

    adiciona header a pagina Wallet - Requisito 05

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import trybeWalletLogo from '../images/logo-trybe-wallet.png';
+import './wallet.css';
 
 class Wallet extends React.Component {
+  renderHeader() {
+    const { email } = this.props;
+    return (
+      <header className="header-wallet">
+        <img
+          className="logo-wallet"
+          src={ trybeWalletLogo }
+          alt="trybe-wallet-logo"
+        />
+
+        <div className="info-wallet">
+          <div className="email-wallet">
+            Email:
+            {' '}
+            <span data-testid="email-field">{email}</span>
+          </div>
+
+          <div className="expenses-wallet">
+            Despesa Total:
+            {' '}
+            <span data-testid="total-field">{0}</span>
+            {' '}
+            <span data-testid="header-currency-field">BRL</span>
+          </div>
+
+        </div>
+      </header>
+    );
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div className="wallet-page">
+        {this.renderHeader()}
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
