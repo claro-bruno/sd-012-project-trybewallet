42810f62382f9e6e31141e75c5a9bc896b942048
camila-mp <ca.malvp@gmail.com>
2021-07-30 16:01:19 -0300

    implementa form e botao no componente Login (requisito 1)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,45 @@
 import React from 'react';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      senha: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, senha } = this.state;
+    return (
+      <div>
+        Login
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input
+              id="email"
+              type="text"
+              name="email"
+              value={ email }
+            />
+          </label>
+          <label htmlFor="senha">
+            Senha:
+            <input
+              id="senha"
+              type="password"
+              name="senha"
+              value={ senha }
+            />
+          </label>
+        </form>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
