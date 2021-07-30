aaac3cba1321aa40e6daf856279fbe773164575f
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-07-30 14:37:41 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,50 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(){
+    super()
+    this.state {
+      email: ''
+      password: ''
+      willredirect: false
+    }
+  }
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="login">
+        <section className="login-container">
+          <label htmlFor="email">
+            E-mail:
+            <input
+              type="text"
+              data-testid="email-input"
+              name="email"
+              value={ email }
+              onChange={}
+            />
+          </label> 
+
+          <label htmlFor="password">
+            Senha:
+            <input
+              type="password"
+              data-testid="password-input"
+              name="password"
+              value={ password }
+              onChange={ }
+            />
+          </label>
+             
+          <button
+            type="button"
+            className="login-button"
+            onClick={}
+          >
+            Entrar
+          </button>
+        </section>
+      </div>
+    )
   }
 }
 
