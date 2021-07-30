4535f8df89fbe9438e630b9d43878348bcd8161c
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-07-30 15:44:19 -0300

    req 4. Crie uma página para sua carteira

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <Switch>
+      <Route exact path="/carteira" component={ Wallet } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
