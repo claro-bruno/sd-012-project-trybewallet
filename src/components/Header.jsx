3d07ef0e99ddb10bf0d9fcfcc08ac176f2a91210
alestori <alestori7@gmail.com>
2021-07-30 19:09:10 -0300

    3. corrigindo erro do eslint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-const Header = ({ email, total, currency }) => {
-  return (
-    <header>
-      <h1>TrybeWallet</h1>
-      <h3 data-testid="email-field">{email}</h3>
-      <p data-testid="total-field">{total !== undefined ? total : 0 }</p>
-      <p data-testid="header-currency-field">{currency}</p>
-    </header>
-  );
-};
+const Header = ({ email, total, currency }) => (
+  <header>
+    <h1>TrybeWallet</h1>
+    <h3 data-testid="email-field">{email}</h3>
+    <p data-testid="total-field">{total !== undefined ? total : 0 }</p>
+    <p data-testid="header-currency-field">{currency}</p>
+  </header>
+);
 
 const mapStateToProps = ({ user, wallet }) => ({
   email: user.email,
