48d4b9a6bc6451536a81e0fdd98fca8114d7de46
MIkaela Braga <bragamikaela@gmail.com>
2021-07-30 15:12:15 -0300

    Requisito 1 com lint revisado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="input-email">
+            <input
+              type="text"
+              data-testid="email-input"
+              placeholder="Insira seu email"
+            />
+          </label>
+          <label htmlFor="input-password">
+            <input
+              type="password"
+              data-testid="password-input"
+              placeholder="Insira sua senha"
+            />
+          </label>
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+// const INITIAL_STATE_WALLET = {
+//   wallet,
+// };
+
+// function wallet(state = INITIAL_STATE_WALLET, action) {
+//   switch (key) {
+//   case value:
+//   default:
+//     return state;
+//   }
+// }
+
+// export default wallet;
