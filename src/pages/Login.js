dfd4550eb22d6a72a58c76f809bf38ed96062a18
Matheus Gonçalves <55060810+matheusgbl@users.noreply.github.com>
2021-07-30 13:55:50 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, password, disabled } = this.state;
+
+    const validation = /^\S+@\S+\.\S+$/;
+    const minLength = 6;
+    const validateEmail = validation.test(email);
     return (
       <div>
         <h2>Login</h2>
         <form>
-          <input placeholder="E-mail" data-testid="email-input" type="email" />
-          <input placeholder="Senha" data-testid="password-input" type="password" />
-          <button type="submit">Entrar</button>
+          <input
+            name="email"
+            placeholder="E-mail"
+            data-testid="email-input"
+            type="email"
+            value={ email }
+            onChange={ this.handleChange }
+            required
+          />
+          <input
+            name="password"
+            minLength="6"
+            placeholder="Senha"
+            data-testid="password-input"
+            type="password"
+            value={ password }
+            onChange={ this.handleChange }
+            required
+          />
+          <Link to="/carteira">
+            {validateEmail && password.length >= minLength && disabled
+              ? (
+                <button
+                  type="submit"
+                >
+                  Entrar
+                </button>
+              ) : (
+                <button
+                  disabled
+                  type="submit"
+                >
+                  Entrar
+                </button>
+              )}
+          </Link>
         </form>
       </div>
     );
