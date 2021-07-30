458786c997d6a10c2104c9ce54b5ba8005b2821f
Adryan Rosa <adryanrosa374@gmail.com>
2021-07-30 19:22:23 -0300

    Troca history.push por Redirect

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Input from '../components/Input';
 import { login as loginAction } from '../actions';
@@ -13,6 +14,7 @@ class Login extends React.Component {
       email: '',
       password: '',
       isDisabled: true,
+      redirect: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -26,10 +28,7 @@ class Login extends React.Component {
     const { login } = this.props;
 
     login({ email });
-    this.setState({
-      email: '',
-      password: '',
-    });
+    this.setState({ redirect: true });
   }
 
   handleChange({ target: { value, name } }) {
@@ -50,50 +49,49 @@ class Login extends React.Component {
   }
 
   render() {
-    const { email, password, isDisabled } = this.state;
+    const { email, password, isDisabled, redirect } = this.state;
 
     return (
-      <main>
-        <div className="card ml-auto mr-auto" style={ { width: '18rem' } }>
-          <img
-            src="https://img.freepik.com/free-vector/brown-leather-wallet-with-lots-money_68708-304.jpg?size=626&ext=jpg"
-            className="card-img-top"
-            alt="Wallet"
-          />
-          <div className="card-body">
-            <form
-              className="d-flex flex-column"
-              onSubmit={ (event) => this.handleSubmit(event) }
+      <div className="card ml-auto mr-auto" style={ { width: '18rem' } }>
+        <img
+          src="https://img.freepik.com/free-vector/brown-leather-wallet-with-lots-money_68708-304.jpg?size=626&ext=jpg"
+          className="card-img-top"
+          alt="Wallet"
+        />
+        <div className="card-body">
+          <form
+            className="d-flex flex-column"
+            onSubmit={ (event) => this.handleSubmit(event) }
+          >
+            <Input
+              label="Email: "
+              id="email-input"
+              name="email"
+              type="email"
+              value={ email }
+              handleChange={ this.handleChange }
+              isValid={ this.isValid }
+            />
+            <Input
+              label="Password: "
+              id="password-input"
+              name="password"
+              type="text"
+              value={ password }
+              handleChange={ this.handleChange }
+              isValid={ this.isValid }
+            />
+            <button
+              className="btn btn-primary mt-3"
+              type="submit"
+              disabled={ isDisabled }
             >
-              <Input
-                label="Email: "
-                id="email-input"
-                name="email"
-                type="email"
-                value={ email }
-                handleChange={ this.handleChange }
-                isValid={ this.isValid }
-              />
-              <Input
-                label="Password: "
-                id="password-input"
-                name="password"
-                type="text"
-                value={ password }
-                handleChange={ this.handleChange }
-                isValid={ this.isValid }
-              />
-              <button
-                className="btn btn-primary mt-3"
-                type="submit"
-                disabled={ isDisabled }
-              >
-                Entrar
-              </button>
-            </form>
-          </div>
+              Entrar
+            </button>
+            {redirect ? <Redirect to="/carteira" /> : ''}
+          </form>
         </div>
-      </main>
+      </div>
     );
   }
 }
