98730839a5108b6af4f6956dcf12661d28a2351e
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-07-30 17:02:34 -0300

    feat fetch api

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,7 +2,28 @@ import React from 'react';
 import Header from '../components/Header';
 import Input from '../components/Input';
 import Select from '../components/Select';
+import { fetchAll } from '../services/api';
+
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currenciesTag: [], 
+    };
+    this.currenciesToState = this.currenciesToState.bind(this);
+  }
+
+  currenciesToState(obj) {
+    const currencies = Object.keys(obj);
+    currencies.splice(currencies.indexOf('USDT'), 1);
+    this.setState({ currenciesTag: currencies });
+  };
+
+  async componentDidMount() {
+    const response = await fetchAll();
+    this.currenciesToState(response);
+  }
   render() {
     return (
     <div>
---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+export const fetchAll = async () => {
+  const URL = 'https://economia.awesomeapi.com.br/json/all';
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
+};
