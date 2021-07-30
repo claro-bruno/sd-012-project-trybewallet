7be6f7a7527130933ac583e0479fcdebc9fd8870
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-07-30 15:56:07 -0300

    cria action para guardar email de login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
-// Coloque aqui suas actions
+import { LOGIN } from './types';
+
+const login = (email) => ({
+  type: LOGIN,
+  email,
+});
+
+export default login;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,4 +1,3 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 const INITIAL_STATE = {
   wallet: {
     currencies: [],
