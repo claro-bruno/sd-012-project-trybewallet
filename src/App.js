8e74b5e18c5f51150dbb983f819ecfb13a637f6e
Erick Silva <erick.silva6653@gmail.com>
2021-07-30 14:23:11 -0300

    implementa validação de login e salva email na store - requisitos 2 e 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Login } from './pages';
+import './App.css';
 
 function App() {
   return (
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,74 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import actionSaveEmail from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      validEmail: true,
+      validPassword: true,
+      redirect: false,
+      activeBtn: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.validate = this.validate.bind(this);
+  }
+
+  validate() {
+    const { email, password } = this.state;
+    const emailValidate = /(.+)@(.+)\.(.+)/;
+    const passwordValidate = /(.+){5,25}/;
+    if (!emailValidate.test(email)) {
+      this.setState({ validEmail: false, activeBtn: false });
+      return false;
+    }
+    if (!passwordValidate.test(password)) {
+      this.setState({ validPassword: false, activeBtn: false });
+      return false;
+    }
+    this.setState({ activeBtn: true, validPassword: true, validEmail: true });
+    return true;
+  }
+
+  handleChange({ target }) {
+    const { value, name } = target;
+    this.setState({ [name]: value, activeBtn: this.validate() });
+  }
+
+  handleClick(evt) {
+    evt.preventDefault();
+    const { saveEmail } = this.props;
+    const { email } = this.state;
+    if (this.validate()) {
+      saveEmail(email);
+      this.setState({ redirect: true });
+    }
+  }
+
   render() {
+    const {
+      email, password, redirect, activeBtn, validEmail, validPassword,
+    } = this.state;
+    if (redirect) { return <Redirect to="/carteira" />; }
     return (
       <section className="login-container">
         <form action="#" className="login-form">
           <label htmlFor="email">
             Email
             <input
-              type="email"
+              type="text"
               placeholder="email@exemplo.com"
               data-testid="email-input"
-              className="login-input"
+              className={ validEmail ? 'login-input' : 'invalid login-input' }
+              name="email"
+              value={ email }
+              onChange={ this.handleChange }
             />
           </label>
           <label htmlFor="password">
@@ -21,15 +76,29 @@ class Login extends React.Component {
             <input
               type="password"
               data-testid="password-input"
-              className="login-input"
+              className={ validPassword ? 'login-input' : 'invalid login-input' }
+              name="password"
+              value={ password }
+              onChange={ this.handleChange }
             />
           </label>
-          <button type="submit" className="login-btn">Entrar</button>
+          <button
+            type="submit"
+            className="login-btn"
+            onClick={ this.handleClick }
+            disabled={ !activeBtn }
+          >
+            Entrar
+          </button>
         </form>
       </section>
     );
   }
 }
+
+Login.propTypes = {
+  saveEmail: PropTypes.func.isRequired,
+};
 
 const mapDispatchToProps = (dispatch) => ({
   saveEmail: (payload) => dispatch(actionSaveEmail(payload)),
