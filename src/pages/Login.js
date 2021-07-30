255989b14a0445008e1e54da1d32bfbff50a46d5
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2021-07-30 17:13:18 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,48 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <section>
+        <h1>Login</h1>
+        <form>
+          <label htmlFor="email">
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={ email }
+              placeholder="Email"
+              data-testid="email-input"
+            />
+          </label>
+          <label htmlFor="password">
+            <input
+              type="text"
+              name="password"
+              id="password"
+              value={ password }
+              placeholder="Password"
+              data-testid="password-input"
+            />
+          </label>
+          <button
+            type="button"
+          >
+            Entrar
+          </button>
+        </form>
+      </section>
+    );
   }
 }
 
