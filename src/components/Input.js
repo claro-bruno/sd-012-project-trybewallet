240f1c69c33bff1cdf53ada121bdda09ef38be19
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-30 14:52:08 -0300

    criando componente Input

---
+++ src/components/Input.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Input extends React.Component {
+  render() {
+    const { label, type, name, value, onChange } = this.props;
+    return (
+      <label htmlFor={ name }>
+        { label }
+        <input
+          name={ name }
+          type={ type }
+          value={ value }
+          onChange={ onChange }
+        />
+      </label>
+    );
+  }
+}
+
+Input.propTypes = {
+  label: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+Input.defaultProps = {
+  label: '',
+};
+
+export default Input;
