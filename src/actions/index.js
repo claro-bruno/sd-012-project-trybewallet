dfeb14aed14ea044b9c7d28bf257c81c1dee1552
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-07-30 16:16:01 -0300

    Feito os reducers de forma default do user e wallet, feito validacao do email e senha

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,23 @@
 // Coloque aqui suas actions
+// import charAPI from "../services/charAPI";
+
+// export const searchBegin = (state) => (
+//   { type: 'SEARCH_BEGIN', loading: true, state }
+// );
+
+// export const searchSuccess = (state) => (
+//   { type: 'SEARCH_SUCCESS', loading: false, state }
+// );
+
+// export const searchFailure = (state) => (
+//   { type: 'SEARCH_ERROR', loading: false, state }
+// );
+
+// export function findCharacter(name) {
+//   return (dispatch) => {
+//     dispatch(searchBegin(name));
+//     return charAPI(name)
+//       .then((json) => dispatch(searchSuccess(json)))
+//       .catch((error) => dispatch(searchFailure(error.message)));
+//   };
+// };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,29 +7,47 @@ class Login extends React.Component {
     this.state = {
       email: '',
       password: '',
+      buttonDisabled: true,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.validation = this.validation.bind(this);
   }
 
   handleSubmit(e) {
     e.preventDefault();
   }
 
+  validation() {
+    const passwordMinimalLen = 6;
+    const re = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const { password, email } = this.state;
+    if (password.length >= passwordMinimalLen && email.match(re)) {
+      this.setState({
+        buttonDisabled: false,
+      });
+    } else {
+      this.setState({
+        buttonDisabled: true,
+      });
+    }
+  }
+
   handleChange({ target }) {
     this.setState({
       [target.name]: target.value,
-    });
+    }, () => this.validation());
   }
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, buttonDisabled } = this.state;
     return (
       <form onSubmit={ this.handleSubmit }>
         <label htmlFor="email-input">
           E-mail
           <input
+            required
             name="email"
             value={ email }
             data-testid="email-input"
@@ -40,6 +58,7 @@ class Login extends React.Component {
         <label htmlFor="password-input">
           Password
           <input
+            required
             name="password"
             value={ password }
             data-testid="password-input"
@@ -47,7 +66,7 @@ class Login extends React.Component {
             onChange={ this.handleChange }
           />
         </label>
-        <button type="submit">Entrar</button>
+        <button disabled={ buttonDisabled } type="submit">Entrar</button>
       </form>
     );
   }
