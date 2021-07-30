1a91fab7d3913294071417cdd649244862328ae0
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-07-30 16:51:13 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,31 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="email">
+          Email
+          <input
+            data-testid="email-input"
+            type="email"
+            id="email"
+          />
+        </label>
+        <label htmlFor="senha">
+          Senha
+          <input
+            data-testid="password-input"
+            type="password"
+            id="senha"
+          />
+        </label>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
