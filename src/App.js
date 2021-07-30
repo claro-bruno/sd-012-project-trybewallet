22a6879c356ffa0c61f9f16e4173f524971dd467
Erick Silva <erick.silva6653@gmail.com>
2021-07-30 14:46:48 -0300

    renderiza o componente "Wallet" - requisito 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Login } from './pages';
+import { Login, Wallet } from './pages';
 import './App.css';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="carteira" component={ Wallet } />
     </Switch>
   );
 }
