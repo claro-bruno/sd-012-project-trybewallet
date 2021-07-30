284c6a1e937d702ed55af08e00d278bbe302177d
Rafael Primon <rafaelfernandesprimon@gmail.com>
2021-07-30 19:42:39 -0300

    req 4 - adiciona rota no app para componente wallet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 import './App.css';
 
 function App() {
   return (
     <Switch>
-      <Route path="/" component={ Login } />
+      <Route exact path="/" component={ Login } />
+      <Route path="/carteira" component={ Wallet } />
     </Switch>
   );
 }
