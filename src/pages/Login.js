f9b8138d0ff2bab79c735a3d9a502186eacc9c3b
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-07-30 14:23:26 -0300

    req 1. Crie uma página inicial de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { func } from 'prop-types';
+import { addUser } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { value, name } = target;
+    this.setState({ [name]: value });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    const { add } = this.props;
+    const passwordMinLength = 6;
+    const regExp = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i; // source regExp: https://mailtrap.io/blog/react-native-email-validation/
+    const { email, password } = this.state;
+    if (regExp.test(email) && password.length >= passwordMinLength) {
+      add(email);
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="email-input">
+          Email:
+          <input
+            id="email-input"
+            name="email"
+            data-testid="email-input"
+            type="email"
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="pass-input">
+          Password:
+          <input
+            id="pass-input"
+            name="password"
+            data-testid="password-input"
+            type="password"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button type="button" onClick={ this.handleClick }>Entrar</button>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  add: (user) => dispatch(addUser(user)),
+});
+
+Login.propTypes = {
+  add: func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { ADD_USER } from '../actions';
+
+const INITIAL = {
+  email: undefined,
+};
+
+const user = (state = INITIAL, action) => {
+  switch (action.type) {
+  case ADD_USER:
+    return { email: action.email };
+  default:
+    return state;
+  }
+};
+
+export default user;
