21dfe52c1d9f98bf9b3c0ff40750dd761da1bd2f
Elias-Forte <eliasforte.tur@gmail.com>
2021-07-30 18:22:49 -0300

    requirement 2

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,14 +3,17 @@ import propTypes from 'prop-types';
 
 class Input extends Component {
   render() {
-    const { type, message } = this.props;
+    const { type, message, handleChange, value } = this.props;
     return (
       <div>
         <label htmlFor={ type }>
           <input
             data-testid={ ` ${message}-input ` }
             type={ type }
+            name={ type }
+            value={ value }
             placeholder={ message }
+            onChange={ handleChange }
           />
         </label>
       </div>
@@ -18,9 +21,11 @@ class Input extends Component {
   }
 }
 
-export default Input;
-
 Input.propTypes = {
   type: propTypes.string.isRequired,
+  value: propTypes.string.isRequired,
   message: propTypes.string.isRequired,
+  handleChange: propTypes.func.isRequired,
 };
+
+export default Input;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,58 @@
 import React from 'react';
+// import { connect } from 'react-redux';
+import Input from '../components/Input';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validation = this.validation.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  validation() {
+    const { email, password } = this.state;
+    const emailVerify = /\S+@\S+\.\S+/;
+    const passwordFormat = 6;
+    if (emailVerify.test(email) && password.length >= passwordFormat) {
+      return false;
+    }
+    return true;
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState(({ [name]: value }), () => ({
+      disable: this.validation(),
+    }));
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <Input
+          type="email"
+          message="email"
+          value={ email }
+          handleChange={ this.handleChange }
+        />
+        <Input
+          type="password"
+          value={ password }
+          message="password"
+          handleChange={ this.handleChange }
+        />
+        <button type="submit" disabled={ this.validation() }>
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
