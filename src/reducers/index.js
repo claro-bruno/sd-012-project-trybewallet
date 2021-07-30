f1760261f5b7ecfb66421d4af04bf538e5226679
Ramon Paschoal <rpaschoal90@gmail.com>
2021-07-30 18:40:50 -0300

    Requisito 4 e 5 refeito com o rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,43 +1,7 @@
-// import user from './user';
-// import wallet from './wallet';
+import { combineReducers } from 'redux';
+import user from './user';
+import wallet from './wallet';
 
-import { LOGIN_USER, WALLET } from '../actions';
+const rootReducer = combineReducers({ user, wallet });
 
-// Configure os seus reducers.
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
-const INITIAL_STATE = {
-  user: {
-    email: '',
-  },
-  wallet: {
-    currencies: [],
-    expenses: [],
-  },
-};
-
-const reducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-  case LOGIN_USER:
-    return {
-      ...state,
-      user: {
-        email: action.payload,
-      },
-    };
-  case WALLET:
-    return {
-      ...state,
-      user: {
-        email: action.payload,
-      },
-      wallet: {
-        currencies: [],
-        expenses: [],
-      },
-    };
-  default:
-    return state;
-  }
-};
-
-export default reducer;
+export default rootReducer;
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,41 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN_USER, WALLET } from '../actions';
+
+// Configure os seus reducers.
+// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case LOGIN_USER:
+    return {
+      ...state,
+      user: {
+        email: action.payload,
+      },
+    };
+  case WALLET:
+    return {
+      ...state,
+      user: {
+        email: action.payload,
+      },
+      wallet: {
+        currencies: [],
+        expenses: [],
+      },
+    };
+  default:
+    return state;
+  }
+};
+
+export default reducer;
