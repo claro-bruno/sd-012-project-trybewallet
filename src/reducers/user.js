c33d14fa32baec135696fb453a0e18729d2725a6
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-30 15:30:20 -0300

    configura reducer user

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,12 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+function user(state = INITIAL_STATE, action) {
+  const { type } = action;
+  switch (type) {
+  default: return state;
+  }
+}
+
+export default user;
