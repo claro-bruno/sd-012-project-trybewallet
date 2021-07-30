32fb833815c2a7e7033c0f00a43e5a5111cd6af4
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2021-07-30 17:51:14 -0300

    Resolve requisito 5 - Cria um Header na pagina Wallet e renderiza email, despesas e moeda

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-class Wallet extends React.Component {
+class Wallet extends Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email, currencies, expenses } = this.props;
+    console.log(currencies, expenses);
+
+    return (
+      <div>
+        <header>
+          <p data-testid="email-field">
+            Usuário:
+            { email }
+          </p>
+          <p data-testid="total-field">
+            Despesa Total: 0
+            {/* { expenses } */}
+          </p>
+          <p data-testid="header-currency-field">
+            Moeda: BRL
+            {/* { currencies } */}
+          </p>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+  currencies: PropTypes.arrayOf().isRequired,
+  expenses: PropTypes.arrayOf().isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  currencies: state.wallet.currencies,
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
