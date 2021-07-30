b1e4739bddf6740b501222c24824baf3f6cd8373
Rafael Cardoso <rafael.vimur@gmail.com>
2021-07-30 17:21:51 -0300

    ajsutes no App.js e adicionando o requisito 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <Route path="/" component={ Login } />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  validation() {
+    const { email, password } = this.state;
+    const emailRegex = email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i);
+    const minValue = 6;
+    if (password.length >= minValue && emailRegex) {
+      return false;
+    }
+
+    return true;
+  }
+
   render() {
     return (
       <div>
@@ -9,13 +37,22 @@ class Login extends Component {
           type="text"
           data-testid="email-input"
           placeholder="email"
+          name="email"
+          onChange={ this.onChange }
         />
         <input
           type="text"
           data-testid="password-input"
           placeholder="password"
+          name="password"
+          onChange={ this.onChange }
         />
-        <button type="button">Entrar</button>
+        <button
+          disabled={ this.validation() }
+          type="button"
+        >
+          Entrar
+        </button>
       </div>
     );
   }
