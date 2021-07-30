f17c77b010dd4bda60a1cd04c9a48af968301f77
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-07-30 16:03:21 -0300

    Req 1 - Criando pagina inicial de login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import wallet from './pages/Wallet';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ wallet } />
+          <Route path="" component="" />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,7 +2,38 @@ import React from 'react';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        TrybeWallet
+        <form>
+          <label htmlFor="email">
+            email:
+            <input
+              data-testid="email-input"
+              type="email"
+              id="email"
+              name=""
+              // value=""
+            />
+          </label>
+          <label htmlFor="senha">
+            senha:
+            <input
+              data-testid="password-input"
+              type="password"
+              id="senha"
+              name=""
+              // value=""
+            />
+          </label>
+        </form>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
