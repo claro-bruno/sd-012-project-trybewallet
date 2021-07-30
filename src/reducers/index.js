030e9e78acde272a8fb6a547c78bdd99aeeec9d3
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-30 15:16:26 -0300

    criando combineReducers e exportando para store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
-// import user from './user';
+import user from './user';
+import { combineReducers } from 'redux';
 // import wallet from './wallet';
 
+const reducer = combineReducers({user});
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+export default reducer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from '../reducers';
 
-const store = createStore(() => {},composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
