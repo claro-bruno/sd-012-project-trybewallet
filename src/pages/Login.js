32d6ae50d93ecc7dcab69402165940f7c6b52cfc
Thalles Carneiro <thallescarneiro@gmail.com>
2021-07-30 15:32:19 -0300

    Requisito 1 - Cria uma página inicial de Login
    Com os campos de e-mail e senha, além de montar a configuração inicial do Redux

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,74 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      redirect: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState((prevState) => ({
+      ...prevState,
+      email: '',
+      password: '',
+      redirect: true,
+    }));
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, redirect } = this.state;
+    if (redirect) { return <Redirect to="/carteira" />; }
+    return (
+      <main>
+        <form onSubmit={ this.handleSubmit }>
+          <h1>Trybe Wallet</h1>
+          <label htmlFor="email-input">
+            <input
+              id="email-input"
+              name="email"
+              type="email"
+              value={ email }
+              data-testid="email-input"
+              placeholder="E-mail"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="password-input">
+            <input
+              id="password-input"
+              name="password"
+              type="password"
+              value={ password }
+              data-testid="password-input"
+              placeholder="Senha"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="submit"
+          >
+            Entrar
+          </button>
+        </form>
+      </main>
+    );
   }
 }
 
