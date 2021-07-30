a9dc9bd62e0d8a8fc432307b39b4190da1983e62
Renata Nunes <renatanunes231@yahoo.com.br>
2021-07-30 15:52:31 -0300

    Pagina inicial de login criada

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import './App.css';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,63 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.submitLogin = this.submitLogin.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  submitLogin(e) {
+    e.preventDefault();
+
+    this.setState({
+      email: '',
+      password: '',
+    });
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    return (
+      <fieldset>
+        <form
+          onSubmit={ this.submitLogin }
+        >
+          <input
+            type="email"
+            data-testid="email-input"
+            placeholder="Email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            placeholder="Senha"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </fieldset>
+    );
   }
 }
 
