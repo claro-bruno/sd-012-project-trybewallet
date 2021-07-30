7833f5556b3073eb0b638e0aee3b3cedf8714573
Ramon Paschoal <rpaschoal90@gmail.com>
2021-07-30 17:45:50 -0300

    Requisito 04

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/carteira" component={ Wallet } />
       </Switch>
     );
   }
