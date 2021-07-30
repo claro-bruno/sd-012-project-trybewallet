5ca8bb7da913e64469b8fbc4cf694320f0a94549
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-30 18:18:22 -0400

    cria variaveis para o tipo da action e cria novas actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,39 @@
+export const USER_EMAIL = 'user-email';
+export const NEW_EXPENSE = 'new-expense';
+export const DELETE_EXPENSE = 'delete-expense';
+
 const userLogin = (userEmail) => ({
-  type: 'userLogin',
+  type: USER_EMAIL,
   user: {
     email: userEmail,
   },
 });
 
-export default userLogin;
+const createNewExpense = (expense) => ({
+  type: NEW_EXPENSE,
+  expense,
+});
+
+const fetchApi = (expense) => async (dispatch) => {
+  const URL = 'https://economia.awesomeapi.com.br/json/all';
+
+  const fetchApiResquest = await fetch(URL);
+  const jsonResponse = await fetchApiResquest.json();
+
+  dispatch(createNewExpense({
+    ...expense,
+    exchangeRates: jsonResponse,
+  }));
+};
+
+const deleteExpense = (expenseToDelete) => ({
+  type: DELETE_EXPENSE,
+  expenseToDelete,
+});
+
+export default {
+  userLogin,
+  fetchApi,
+  createNewExpense,
+  deleteExpense,
+};
