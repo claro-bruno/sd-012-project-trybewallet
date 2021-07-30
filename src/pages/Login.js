59ba814b3877327d98f38268e927f6eef3010b4d
Matheus Proença <mproenca.marketing@gmail.com>
2021-07-30 18:27:53 -0300

    Verificação de Email e Senha - Req:02

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,69 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      emailIsValid: false,
+      passwordIsValid: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // consulta ao repositorio do Miguel retroz sobre validação;
+
+  emailChecker(value) {
+    const emailRegex = new RegExp([
+      '^[a-zA-Z0-9.!#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9]',
+      '(?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])',
+      '?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$'].join(''));
+    const emailIsValid = emailRegex.test(value);
+    return emailIsValid;
+  }
+
+  passChecker(value) {
+    const minChars = 6;
+    const passIsValid = value.length >= minChars;
+    return passIsValid;
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    const valid = (name === 'email') ? this.emailChecker(value) : this.passChecker(value);
+    this.setState({
+      [name]: value,
+      [`${name}IsValid`]: valid,
+    });
+  }
+
   render() {
+    const { email, password, emailIsValid, passwordIsValid } = this.state;
+
     return (
       <form>
         <input
           data-testid="email-input"
           type="text"
+          name="email"
           placeholder="Email"
+          value={ email }
+          onChange={ this.handleChange }
         />
 
         <input
           data-testid="password-input"
           type="password"
+          name="password"
           placeholder="Password"
+          value={ password }
+          onChange={ this.handleChange }
         />
-        <button type="submit">
+        <button
+          type="submit"
+          disabled={ !(emailIsValid && passwordIsValid) }
+        >
           Entrar
         </button>
       </form>
