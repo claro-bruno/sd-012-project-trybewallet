2fc720ded5f15ff0e943fcb3bb8f80b8e5ef3829
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-07-30 18:33:22 -0300

    Resolve requisito 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+export const SAVE_LOGIN_NAME = 'SAVE_LOGIN_NAME';
+
+export const saveLoginName = (payload) => ({
+  type: SAVE_LOGIN_NAME,
+  payload,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { saveLoginName } from '../actions';
 
 class Login extends React.Component {
   constructor() {
@@ -6,32 +10,94 @@ class Login extends React.Component {
     this.state = {
       email: '',
       password: '',
+      activateLogin: true,
+
     };
+    this.handleChangeInLogin = this.handleChangeInLogin.bind(this);
+    this.letsStart = this.initStart.bind(this);
+  }
+
+  // manipulando input login
+  handleChangeInLogin({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    this.activateBtnLogin();
+  }
+
+  initStart() {
+    const { importLogin } = this.props;
+    const { email } = this.state;
+    importLogin(email);
+    this.setState({
+      shouldRedirectToCarteira: true,
+    });
+  }
+
+  activateBtnLogin() {
+    const { email, password } = this.state;
+    const minimumPw = 5;
+    const checkEmail = /.+@.+\.[A-Za-z]+$/;
+    if (password.length >= minimumPw && checkEmail.test(email)) {
+      this.setState({
+        activateLogin: false,
+      });
+    } else {
+      this.setState({
+        activateLogin: true,
+      });
+    }
   }
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, activateLogin,
+      shouldRedirectToCarteira } = this.state;
+
+    if (shouldRedirectToCarteira) {
+      return <Redirect to="/carteira" />;
+    }
     return (
       <div>
-        <h1>Login</h1>
-        ;
-        <form>
+        <label htmlFor="email">
+          E-mail:
           <input
             data-testid="email-input"
             value={ email }
             type="text"
             name="email"
+            onChange={ this.handleChangeInLogin }
           />
+        </label>
+
+        <label htmlFor="password">
+          Senha:
           <input
             data-testid="password-input"
-            vaçue={ password }
+            value={ password }
             id="password"
             name="password"
+            onChange={ this.handleChangeInLogin }
           />
-          <button type="button">Entrar</button>
-        </form>
+        </label>
+        <button
+          type="button"
+          disabled={ activateLogin }
+          onClick={ this.initStart }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
 }
-export default Login;
+
+const mapDispatchToProps = (dispatch) => ({
+  importLogin: (state) => dispatch(saveLoginName(state)),
+});
+
+Login.propTypes = {
+  importLogin: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
