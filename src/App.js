0d3085e14b9f0e4b3d9e42325ea44b3d6558e744
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-07-30 16:20:30 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
-// inicio de projeto 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Wallet</h1>
+        <form>
+          <label htmlFor="email-input">
+            Email:
+            <input
+              type="email"
+              placeholder="Insira seu email"
+              data-testid="email-input"
+            />
+          </label>
+          <label htmlFor="password-input">
+            Senha:
+            <input
+              type="email"
+              placeholder="Digite sua senha"
+              data-testid="password-input"
+              disabled
+            />
+          </label>
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
