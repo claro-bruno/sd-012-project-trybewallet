a97e300390fddaec462821cfb4c359b305047e9f
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2021-07-30 18:37:26 -0300

    Requisito 2 - Verificacoes nos elementos do form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,33 +6,73 @@ class Login extends React.Component {
     this.state = {
       emailInputText: '',
       passwordInputText: '',
+      emailIsValid: false,
+      passwordIsValid: false,
+      enableSubmit: false,
     };
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.checkEnableSubmit = this.checkEnableSubmit.bind(this);
+  }
+
+  checkEnableSubmit() {
+    const { emailIsValid, passwordIsValid } = this.state;
+    this.setState({
+      enableSubmit: emailIsValid && passwordIsValid,
+    });
+  }
+
+  handleChangeEmail(event) {
+    const emailPattern = /^[a-z0-9_.-]+@[a-z]+\.[a-z]{2,3}(?:\.[a-z]{2})?$/;
+    this.setState({
+      emailInputText: event.target.value,
+      emailIsValid: emailPattern.test(String(event.target.value)),
+    }, () => (this.checkEnableSubmit()));
+  }
+
+  handleChangePassword(event) {
+    const passwordMinLength = 6;
+    this.setState({
+      passwordInputText: event.target.value,
+      passwordIsValid: (event.target.value.length >= passwordMinLength),
+    }, () => (this.checkEnableSubmit()));
   }
 
   render() {
-    const { emailInputText, passwordInputText } = this.state;
+    const {
+      emailInputText,
+      passwordInputText,
+      enableSubmit,
+    } = this.state;
     return (
       <div>
         <form onSubmit={ this.submitLogin }>
           <label htmlFor="email-input">
             Email:
             <input
-              type="text"
+              type="email"
               id="email-input"
               data-testid="email-input"
               value={ emailInputText }
+              onChange={ this.handleChangeEmail }
             />
           </label>
           <label htmlFor="password-input">
             Senha:
             <input
-              type="text"
+              type="password"
               id="password-input"
               data-testid="password-input"
               value={ passwordInputText }
+              onChange={ this.handleChangePassword }
             />
           </label>
-          <button type="button">Entrar</button>
+          <button
+            type="submit"
+            disabled={ !enableSubmit }
+          >
+            Entrar
+          </button>
         </form>
       </div>
     );
