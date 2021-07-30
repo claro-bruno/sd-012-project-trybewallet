628adb3fa14d5954247691c4cf93292efc7bf8ba
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-07-30 16:24:00 -0300

    Feat: Setting up Store folder

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+
+const extension = window.devToolsExtension() || ((f) => f);
+
+const store = createStore(rootReducer, compose(extension));
+
+export default store;
