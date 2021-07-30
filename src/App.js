fba0fc96225a9eb2211b9316b1515808db947961
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-07-30 16:13:38 -0300

    Resolve o requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Login />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,48 @@
 import React from 'react';
 
 class Login extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {value: ''};
+
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  // }
+
+  // handleChange(event) {
+  //   this.setState({value: event.target.value});
+  // }
+
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  // }
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            // value={this.state.value}
+            // onChange={this.handleChange}
+            data-testid="email-input"
+            id="email"
+            name="email"
+            placeholder="Digite seu e-mail"
+          />
+
+          <input
+            type="text"
+            // value={this.state.value}
+            // onChange={this.handleChange}
+            data-testid="password-input"
+            id="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <input type="submit" value="Entrar" />
+        </form>
+      </div>
+    );
   }
 }
 
