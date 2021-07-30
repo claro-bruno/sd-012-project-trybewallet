0d8525bb9e12b5ce29c48848fbd7fd0cac4ea482
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-07-30 16:21:21 -0300

    feat header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 import { Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
@@ -8,6 +9,9 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/">
             <Login />
+          </Route>
+          <Route path="/carteira">
+            <Wallet />
           </Route>
         </Switch>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      total: 0,
+    };
+  }
+  render() {
+    const { userEmail } = this.props;
+    const { total } = this.state;
+    return (
+      <header>
+        <h1>Weballet</h1>
+        <p data-testid="email-field">{ userEmail }</p>
+        <p data-testid="total-field">{ total }</p>
+        <p data-testid="header-currency-field"></p>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Header.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
