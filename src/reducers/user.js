5d94fefdf634c960bc4bd523b0f7ad0fc85e4c15
Samuel Constantino <samuelicapuidados@gmail.com>
2021-07-31 05:18:22 -0300

    adiciona userReducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,20 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { ADD_EMAIL } from '../actions/userActions';
+
+const INITIAL_STATE = {
+  email: [],
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case ADD_EMAIL: {
+    return {
+      ...state,
+      email: [...state.email, action.payload],
+    };
+  }
+  default: return state;
+  }
+};
+
+export default userReducer;
