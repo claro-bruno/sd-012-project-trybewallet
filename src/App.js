c3ee8a26df17c631bed38517e36751c438a3891f
Martin <escorpmartin97@gmail.com>
2021-07-31 19:10:54 -0300

    [req 4 - Cria rota para wallet]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Login } />
+          <Route exact path="/carteira" component={ Wallet } />
         </Switch>
       </BrowserRouter>
     );
