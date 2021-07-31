cf4e00f624330f16eb31a9f5f9eb6df12ccdd135
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-07-31 11:38:48 -0300

    Refatorado o Header, sendo agora um componente

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const { userEmail } = this.props;
+    return (
+      <header>
+        <p data-testid="email-field">
+          Email:
+          { userEmail }
+        </p>
+        <p data-testid="total-field">
+          Despesa Total:
+          0
+          <span data-testid="header-currency-field">
+            BRL
+          </span>
+        </p>
+      </header>
+    );
+  }
+};
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Header.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,47 +1,24 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import Header from '../components/Header';
 import Input from '../components/Input';
 import Select from '../components/Select';
 import { TAGS, PAYMENT } from '../data';
 
 class Wallet extends React.Component {
   render() {
-    const { userEmail } = this.props;
     return (
       <div>
-        <header>
-          <p data-testid="email-field">
-            Email:
-            { userEmail }
-          </p>
-          <p data-testid="total-field">
-            Despesa Total:
-            0
-            <span data-testid="header-currency-field">
-              BRL
-            </span>
-          </p>
-        </header>
         <form>
+          <Header />
           <Input type="text" name="valor" text="Valor" />
           <Select name="coin" text="Moeda" />
           <Select name="payment" text="Método de Pagamento" content={ PAYMENT } />
           <Input type="text" name="description" text="Descrição" />
           <Select name="tag" text="Tag:" content={ TAGS } />
         </form>
-
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  userEmail: state.user.email,
-});
-
-Wallet.propTypes = {
-  userEmail: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps)(Wallet);
+export default Wallet;
