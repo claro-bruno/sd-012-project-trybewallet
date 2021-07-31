3d4dffb4d7cb09fdcc837f8131387be77cecfba0
cristianocsl <cslcristiano@gmail.com>
2021-07-30 22:18:59 -0300

    Requisito 5: Crie um header para a página de carteira contendo as seguintes características

---
+++ src/pages/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { func } from 'prop-types';
+
+class Header extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    const { emailFromGlobalState } = this.props;
+    return (
+      <header>
+        <h3 data-testid="email-field">
+          {`Email: ${emailFromGlobalState}`}
+        </h3>
+        <label htmlFor="despesa">
+          Despesa:
+          <input
+            data-testid="total-field"
+            name="despesa"
+            value="0"
+          />
+        </label>
+        <div data-testid="header-currency-field">BRL</div>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  emailFromGlobalState: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Header);
+
+Header.propTypes = ({
+  emailFromGlobalState: func.isRequired,
+});
