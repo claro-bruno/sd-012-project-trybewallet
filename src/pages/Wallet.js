1c5a87bea39c82407dabf9ba7700b4bd62fab639
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-31 13:57:11 -0300

    inplementa inputValor na Page Wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import Header from '../components/standart/Header';
+import InputValor from '../components/WalletControlled/InputValor';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      expenseAmount: 0,
+    };
+  }
+
+  handleChange({ target: { name, type, value, checked } }) {
+    function newValue() {
+      switch (type) {
+      case 'checkbox': return checked;
+      case 'number': return +value;
+      default: return value;
+      }
+    }
+    this.setState((state) => ({ ...state, [name]: newValue() }));
+  }
+
   render() {
+    const {
+      state: { expenseAmount },
+      handleChange,
+    } = this;
+
     return (
-      <Header />
+      <>
+        <Header />
+        <InputValor
+          value={ expenseAmount }
+          handleChange={ handleChange }
+        />
+      </>
+
     );
   }
 }
