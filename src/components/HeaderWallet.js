f54ef7f57bb8e7708a75b5e67d404f95c7d18c1e
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-07-30 21:37:05 -0300

    add number formatter

--- src/components/HeaderWallet.js
+++ src/components/HeaderWallet.js
@@ -5,16 +5,24 @@ import { string, arrayOf, shape, number } from 'prop-types';
 class HeaderWallet extends React.Component {
   render() {
     const { email, expenses } = this.props;
-    // const fmt = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
-    const total = expenses.reduce((acc, { value, currency, exchangeRates }) => (
-      acc + parseFloat(exchangeRates[currency].ask) * parseFloat(value)
-    ), 0);
+    const fmt = new Intl.NumberFormat('en-US', {
+      style: 'decimal',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+    const total = expenses.reduce(
+      (acc,
+        {
+          value,
+          currency,
+          exchangeRates,
+        }) => acc + parseFloat(exchangeRates[currency].ask) * parseFloat(value),
+      0,
+    );
     return (
       <header>
         <span data-testid="email-field">{email}</span>
-        <span data-testid="total-field">
-          {total}
-        </span>
+        <span data-testid="total-field">{fmt.format(total)}</span>
         <span data-testid="header-currency-field">BRL</span>
       </header>
     );
@@ -33,11 +41,13 @@ const mapStateToProps = (state) => ({
 
 HeaderWallet.propTypes = {
   email: string.isRequired,
-  expenses: arrayOf(shape({
-    id: number,
-    value: string,
-    currency: string,
-  })).isRequired,
+  expenses: arrayOf(
+    shape({
+      id: number,
+      value: string,
+      currency: string,
+    }),
+  ).isRequired,
 };
 
 export default connect(mapStateToProps)(HeaderWallet);
