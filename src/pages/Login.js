22b8799afa73e6c61435b547a2cc3f4488dba97f
Renan Ferreira <renansantiago2013@hotmail.com>
2021-07-31 18:43:07 -0300

    Resolve o requisito 2 - verificaçoes na tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,67 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './login.css';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.formCheck = this.formCheck.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+    this.formCheck();
+  }
+
+  formCheck() {
+    const { email, password } = this.state;
+    const minLength = 5;
+    const checkEmail = /(.*)@(.*).com/; // Regex tirado do slack e fornecido por Mikaela Braga - Turma 12
+    if (checkEmail.test(email) && password.length >= minLength) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
+    }
+  }
+
   render() {
+    const { email, password, disabled } = this.state;
+
     return (
       <div className="login-container">
         <form>
           <h3>TrybeWallet</h3>
-          <input type="text" name="email" data-testid="email-input" />
-          <input type="password" name="password" data-testid="password-input" />
-          <button type="button">Entrar</button>
+          <input
+            type="email"
+            name="email"
+            data-testid="email-input"
+            value={ email }
+            onChange={ this.handleChange }
+            placeholder="E-mail"
+          />
+          <input
+            type="password"
+            name="password"
+            data-testid="password-input"
+            value={ password }
+            onChange={ this.handleChange }
+            placeholder="Senha"
+          />
+          <Link to="/carteira">
+            <button disabled={ disabled } type="button">
+              Entrar
+            </button>
+          </Link>
         </form>
       </div>
     );
