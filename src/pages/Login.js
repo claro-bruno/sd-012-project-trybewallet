1066faebb95b40179f85cc2e47c7a906047d3ea0
Bruno Augusto <brunaugusto@gmail.com>
2021-07-31 01:40:41 -0300

    Primeiro Requisito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <TextField
+          label="Email"
+          variant="outlined"
+          data-testid="email-input"
+        />
+        <TextField
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          data-testid="password-input"
+          variant="outlined"
+        />
+        <Button variant="outlined" color="primary">
+          Entrar
+        </Button>
+      </form>
+    );
   }
 }
 
