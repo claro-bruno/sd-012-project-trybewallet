f81590cddb3b16fcb0ad468b39ee5ff76bf18968
jmlinger <jmlinger10@gmail.com>
2021-07-31 19:19:19 -0300

    Resolve requisito 2: se email e senha forem validos habilita botao de enviar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,61 @@
 import React from 'react';
 
 class Login extends React.Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.validateEmail = this.validateEmail.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  validateEmail() {
+    const { email, password } = this.state;
+    const re = /^[a-z0-9_.-]+@[a-z]+\.[a-z]{2,3}(?:\.[a-z]{2})?$/;
+
+    const emailTest = re.test(String(email).toLowerCase());
+    const number = 6;
+    const passwordTest = password.length >= number;
+
+    if (emailTest && passwordTest) {
+      return false;
+    }
+    return true;
+  }
+
+  handleOnChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
 
   render() {
+    const { email, password } = this.state;
     return (
       <form className="form-login">
-        <input data-testid="email-input" type="email" placeholder="Email" />
-        <input data-testid="password-input" type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <input
+          data-testid="email-input"
+          type="email"
+          name="email"
+          value={ email }
+          placeholder="Email"
+          onChange={ this.handleOnChange }
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          name="password"
+          value={ password }
+          placeholder="Senha"
+          onChange={ this.handleOnChange }
+        />
+        <button type="submit" disabled={ this.validateEmail() }>Entrar</button>
       </form>);
   }
 }
