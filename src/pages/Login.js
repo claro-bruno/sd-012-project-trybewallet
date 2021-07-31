46658089d4668c0ee1289f977c04f1d72dabe5a8
Djaniza Vasques <djanizav@gmail.com>
2021-07-31 02:19:48 -0300

    Adiciona formulario no arq Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,41 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email:'',
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email } = this.state;
+    const { loginBtn } = this.props;
+    return (
+      <form>
+        <h1>Login</h1>);
+        <input
+        data-testid="email-input"
+        type="email"
+        placeholder="e-mail"
+        value={ email }
+        name="email"
+        />
+        <input
+        data-testid="password-input"
+        type="password"
+        placeholder="senha"
+        name="password"
+        minLength="6"
+        />
+        <button
+        type="submit"
+        onClick={ loginBtn }
+        >
+          Entrar
+        </button>
+      </form>
+    )
   }
 }
 
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,9 @@
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
-const store = createStore(combineReducers(user, wallet), composeWithDevTools);
+const store = createStore(combineReducers(user, wallet), composeWithDevTools(
+    applyMiddleware(thunk),
+),);
 
 export default store;
