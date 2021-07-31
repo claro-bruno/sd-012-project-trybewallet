19b91f39941fb670f7ebbb998742aa435e310bd7
Luiz Henrique <luishenriqueee2010@hotmail.com>
2021-07-31 14:32:34 -0300

    Adiciona requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input type="text" data-testid="email-input" placeholder="E-mail aqui :)" />
+        <input type="text" data-testid="password-input" placeholder="Senha aqui :)" />
+        <button type="button">ENTRAR</button>
+      </div>);
   }
 }
 
---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import allReducer from '../reducers';
+
+const store = createStore(
+  allReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
+
+export default store;
