8919d532a191b0f3f8e0dcb9f0cb0cc330f48b00
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-31 16:03:50 -0300

    cria action getAPICoins

---
+++ src/actions/getApiCoins.js
@@ -0,0 +1,18 @@
+export const GET_COINS = 'GET_COINS';
+export const GET_COINS_SUCCESS = 'GET_COINS_SUCCESS';
+export const GET_COINS_ERROR = 'GET_COINS_ERROR';
+
+export const getCoins = () => ({ type: GET_COINS });
+
+export const getCoinsSuccess = (coins) => ({ type: GET_COINS_SUCCESS, payload: coins });
+
+export const getCoinsError = (error) => ({ type: GET_COINS_ERROR, error });
+
+export const fetchAPI = () => async (dispatch) => {
+  dispatch(getCoins());
+  const END_POINT = 'https://economia.awesomeapi.com.br/json/all';
+  fetch(END_POINT)
+    .then((data) => data.json())
+    .then(({ results }) => dispatch(getCoinsSuccess(results)))
+    .catch((error) => dispatch(getCoinsError(error)));
+};
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,11 @@
 export { ADD_EMAIL, addEmailAction } from './addEmailAction';
+
+export {
+  GET_COINS,
+  GET_COINS_SUCCESS,
+  GET_COINS_ERROR,
+  getCoins,
+  getCoinsSuccess,
+  getCoinsError,
+  fetchAPI,
+} from './getApiCoins';
