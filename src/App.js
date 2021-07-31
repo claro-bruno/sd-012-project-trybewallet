86ffa9a0a2fdcb3833e9ccda5ba587fd84f2a149
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-07-30 22:33:41 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends Component {
   render() {
@@ -8,7 +9,7 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path="/" component={ Login } />
-          {/* <Route path="/carteira" component={ Wallet } /> */}
+          <Route path="/carteira" component={ Wallet } />
         </Switch>
       </div>
     );
