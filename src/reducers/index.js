daaa7833ba8062163ce37ff0ca112774d0cb42a5
Samuel Constantino <samuelicapuidados@gmail.com>
2021-07-31 05:03:24 -0300

    adiciona userActions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 import user from './user';
-import wallet from './wallet';
+// import wallet from './wallet';
 
-const rootReducer = combineReducers({ user, wallet });
+const rootReducer = combineReducers({ user });
 
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 
