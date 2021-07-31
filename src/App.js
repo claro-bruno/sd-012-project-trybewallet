1f68abe23430669edb679c5efbcd067f2fcbb7a1
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-07-31 19:43:20 -0300

    Requisito 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <Switch>
-      {/* <Route path="/carteira" /> */}
+      <Route path="/carteira" component={ Wallet } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
