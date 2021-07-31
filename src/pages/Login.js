e1e6d0eb29c8a302f7bb498790f10c5de04340f1
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-07-31 12:49:09 -0300

    Iniciando o projeto

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="email">
+          Email
+          <input data-testid="email-input" type="text" name="email" />
+        </label>
+        <label htmlFor="senha">
+          Senha
+          <input data-testid="password-input" type="text" name="senha" />
+        </label>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
