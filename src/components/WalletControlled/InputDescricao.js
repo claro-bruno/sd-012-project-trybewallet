5af7d078c8d2ec2ef9aeb9c81c7a749e9934cbfd
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-31 14:10:05 -0300

    cria o component InputDescricao

---
+++ src/components/WalletControlled/InputDescricao.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Input from '../standart/Input';
+
+class InputDescricao extends React.Component {
+  render() {
+    const {
+      props: {
+        value,
+        handleChange,
+      },
+    } = this;
+
+    return (
+      <Input
+        name="expenseAmount"
+        labelText="Descrição:"
+        type="text"
+        value={ value }
+        handleChange={ handleChange }
+        placeholder="Digite a Descrição da Despesa"
+      />
+    );
+  }
+}
+
+const { string, func, number, oneOfType } = PropTypes;
+InputDescricao.propTypes = {
+  value: oneOfType([
+    number,
+    string,
+  ]).isRequired,
+  handleChange: func.isRequired,
+};
+
+export default InputDescricao;
