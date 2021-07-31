839af96f692f56c7831a80dff895e5d0141beb2c
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-31 11:09:58 -0300

    cria component InputEmail

---
+++ src/components/LoginControlled/InputEmail.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Input from '../standart/Input';
+
+class InputEmail extends React.Component {
+  render() {
+    const {
+      props: {
+        value,
+        handleChange,
+      },
+    } = this;
+
+    const emailValidation = value.includes('@' && '.com');
+    const ERROR_EMAIL = 'Email inválido';
+
+    return (
+      <Input
+        name="email"
+        labelText="Email:"
+        type="email"
+        dataTestId="email-input"
+        value={ value }
+        handleChange={ handleChange }
+        placeholder="Digite o Email"
+        validation={ !emailValidation && <span>{ ERROR_EMAIL }</span> }
+      />
+    );
+  }
+}
+
+const { string, func } = PropTypes;
+InputEmail.propTypes = {
+  value: string.isRequired,
+  handleChange: func.isRequired,
+};
+
+export default InputEmail;
