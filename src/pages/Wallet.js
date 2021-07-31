81264c257dbc39399dae13fc1c5f45e270b84b89
Josimar Souza <contact.josimarsouza@gmail.com>
2021-07-31 10:44:25 -0300

    Implementa um header na pagina Wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+
+    return (
+      <section>
+        <header>
+          <h1>TRYBE WALLET</h1>
+          <p data-testid="email-field">{ `Email: ${email}` }</p>
+          <p data-testid="total-field">Despesa Total: 0</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </header>
+      </section>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToPros = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToPros)(Wallet);
