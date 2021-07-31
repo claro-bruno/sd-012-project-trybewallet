2eb2d9fb923aa7502d5b2952f1c63f58f45b539c
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-30 22:03:49 -0300

    adicionando um return em ExpenseTable

--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -6,57 +6,61 @@ class ExpenseTable extends React.Component {
   renderTable() {
     const { expenses } = this.props;
     return (
-      <table>
-        <tbody>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-          { expenses.map((expense, index) => {
-            const {
-              id,
-              description,
-              tag,
-              method,
-              value,
-              currency,
-              exchangeRates,
-            } = expense;
-            const { ask, name } = exchangeRates[currency];
-            const regex = /(.+)\/(\w+)/.exec(name);
-            return (
-              <tr key={ id }>
-                <td>{ description }</td>
-                <td>{ tag }</td>
-                <td>{ method }</td>
-                <td>{ value }</td>
-                <td>{ regex ? regex[1] : name }</td>
-                <td>{ Number(ask).toFixed(2) }</td>
-                <td>{ Number(ask * value).toFixed(2) }</td>
-                <td>{ regex ? regex[2] : 'Real' }</td>
-                <td>
-                  <button type="button">Teste</button>
-                  /
-                  <button type="button" onClick={ () => index }>Teste</button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <tbody>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+        { expenses.map((expense, index) => {
+          const {
+            id,
+            description,
+            tag,
+            method,
+            value,
+            currency,
+            exchangeRates,
+          } = expense;
+          const { ask, name } = exchangeRates[currency];
+          const regex = /(.+)\/(\w+)/.exec(name);
+          return (
+            <tr key={ id }>
+              <td>{ description }</td>
+              <td>{ tag }</td>
+              <td>{ method }</td>
+              <td>{ value }</td>
+              <td>{ regex ? regex[1] : name }</td>
+              <td>{ Number(ask).toFixed(2) }</td>
+              <td>{ Number(ask * value).toFixed(2) }</td>
+              <td>{ regex ? regex[2] : 'Real' }</td>
+              <td>
+                <button type="button">Teste</button>
+                /
+                <button type="button" onClick={ () => index }>Teste</button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
     );
   }
 
   render() {
     const { expenses } = this.props;
-    if (expenses.length > 0) { this.renderTable(); }
+    if (expenses.length > 0) {
+      return (
+        <table>
+          { this.renderTable() }
+        </table>
+      );
+    }
     return null;
   }
 }
