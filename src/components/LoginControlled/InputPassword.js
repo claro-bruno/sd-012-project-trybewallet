55398a3a21146c436831732e5dd59518d70f9a2c
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-31 11:19:17 -0300

    cria component InputPassword

---
+++ src/components/LoginControlled/InputPassword.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Input from '../standart/Input';
+
+class InputPassword extends React.Component {
+  render() {
+    const {
+      props: {
+        value,
+        handleChange,
+      },
+    } = this;
+
+    const VALID_CARACTERES = 6;
+    const passwordValidation = value.length >= VALID_CARACTERES;
+    const ERROR_PASSWORD = 'Password inválido';
+
+    return (
+      <Input
+        name="password"
+        labelText="Password:"
+        type="password"
+        dataTestId="password-input"
+        value={ value }
+        handleChange={ handleChange }
+        placeholder="Digite a Senha"
+        validation={ !passwordValidation && <span>{ ERROR_PASSWORD }</span> }
+      />
+    );
+  }
+}
+
+const { string, func } = PropTypes;
+InputPassword.propTypes = {
+  value: string.isRequired,
+  handleChange: func.isRequired,
+};
+
+export default InputPassword;
