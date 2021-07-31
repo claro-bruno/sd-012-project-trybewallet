989ce9c7916161e2b7bd5a26205b5253fe09f9a4
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-07-30 23:01:13 -0300

    requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Wallet extends React.Component {
+class Wallet extends Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <header>
+          <div>
+            <p>Email:</p>
+            <p data-testid="email-field">.</p>
+          </div>
+          <div>
+            <p>Total gastos: R$</p>
+            <p data-testid="total-field">0</p>
+          </div>
+          <div>
+            <p>Total gastos: R$</p>
+            <p data-testid="header-currency-field">`BRL`</p>
+          </div>
+        </header>
+      </div>
+    );
   }
 }
 
