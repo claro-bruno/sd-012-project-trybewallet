1c64017682bbcff4af4c21dcb649fd2f6340eb30
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-07-31 16:21:50 -0300

    inplementa reducer wallet para receber dados a API

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,12 +1,26 @@
+import { GET_COINS, GET_COINS_SUCCESS, GET_COINS_ERROR } from '../actions';
+
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
+  error: null,
+  isLoading: false,
 };
 
 function wallet(state = INITIAL_STATE, action) {
-  const { type } = action;
+  const { type, payload, error } = action;
   switch (type) {
-  default: return state;
+  case GET_COINS:
+    return { ...state, isLoading: true };
+
+  case GET_COINS_SUCCESS:
+    return { ...state, error: null, characters: payload, isLoading: false };
+
+  case GET_COINS_ERROR:
+    return { ...state, error, isLoading: false };
+
+  default:
+    return state;
   }
 }
 
