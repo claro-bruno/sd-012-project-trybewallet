4e27560f7b0c76b7c9255b847e6ebacf8b006e82
Bruno Augusto <brunaugusto@gmail.com>
2021-07-31 01:49:06 -0300

    Primeiro Requisito Fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 
 class Login extends React.Component {
   render() {
     return (
       <form>
-        <TextField
+        <input
+          type="email"
           label="Email"
-          variant="outlined"
           data-testid="email-input"
         />
-        <TextField
+        <input
           label="Password"
           type="password"
-          autoComplete="current-password"
           data-testid="password-input"
-          variant="outlined"
         />
-        <Button variant="outlined" color="primary">
+        <button
+          type="button"
+          // disabled={ disabled }
+          // onClick={ () => submitEmail(email) }
+        >
           Entrar
-        </Button>
+        </button>
       </form>
     );
   }
