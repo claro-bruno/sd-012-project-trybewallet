91c81f024ddca415a2339a8ae386490a4f92d1eb
RenanMiyashita <renanmiyashita@gmail.com>
2021-07-31 19:26:57 -0300

    cria store, provider, reducer user&&wallet, config reduxDevTools

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'ACCESS_USER':
+    return state;
+  default:
+    return state;
+  }
+}
+
+export default user;
