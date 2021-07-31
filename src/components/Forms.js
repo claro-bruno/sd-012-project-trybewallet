d1deb87742ae785b88f22ce58f9c0583883b057f
Rafael Cardoso <rafael.vimur@gmail.com>
2021-07-31 20:17:03 -0300

    adicioana requisito 6, formulario da page Wallet

---
+++ src/components/Forms.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+
+class Forms extends Component {
+  value() {
+    return (
+      <label htmlFor="input-value">
+        Valor
+        <input
+          type="text"
+          name="name"
+          id="input-value"
+        />
+      </label>
+    );
+  }
+
+  description() {
+    return (
+      <label htmlFor="input-description">
+        Descrição
+        <input
+          type="text"
+          name="name"
+          id="input-description"
+        />
+      </label>
+    );
+  }
+
+  currency() {
+    return (
+      <label htmlFor="input-currency">
+        Moeda
+        <select
+          name="name"
+          id="input-currency"
+        >
+          <option value="brl">BRL</option>
+        </select>
+      </label>
+    );
+  }
+
+  payment() {
+    return (
+      <label htmlFor="input-payment">
+        Método de pagamento
+        <select
+          name="name"
+          id="input-payment"
+        >
+          <option value="dinheiro">Dinheiro</option>
+          <option value="credito">Cartão de crédito</option>
+          <option value="debito">Cartão de débito</option>
+        </select>
+      </label>
+    );
+  }
+
+  tag() {
+    return (
+      <label htmlFor="input-tag">
+        Tag
+        <select
+          name="name"
+          id="input-tag"
+        >
+          <option value="alimentacao">Alimentação</option>
+          <option value="lazer">Lazer</option>
+          <option value="trabalho">Trabalho</option>
+          <option value="transporte">Transporte</option>
+          <option value="saude">Saúde</option>
+        </select>
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <form>
+        {this.value()}
+        {this.description()}
+        {this.currency()}
+        {this.payment()}
+        {this.tag()}
+      </form>
+    );
+  }
+}
+
+export default Forms;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Forms from '../components/Forms';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <Forms />
       </div>
     );
   }
