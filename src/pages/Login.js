d978450f27e4e0f5cb74297edb1f4dcff1864e5c
Álvaro Palma <palma.alvaro@outlook.com>
2021-07-31 15:59:49 -0300

    Cria os campos da pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <h2>Trybe Wallet</h2>
+        <h3>Login de Usuário</h3>
+        <label htmlFor="input-email">
+          Insira seu e-mail:
+          <input
+            id="input-email"
+            type="text"
+            data-testid="email-input"
+          />
+        </label>
+        <label htmlFor="input-password">
+          Insira sua senha:
+          <input
+            id="input-password"
+            type="password"
+            data-testid="password-input"
+          />
+        </label>
+        <button
+          type="submit"
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
