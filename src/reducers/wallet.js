0624fe0c5f715e455604b8b61a51dbc2254b4b50
alestori <alestori7@gmail.com>
2021-07-30 21:01:15 -0300

    corrigindo importação em wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,6 +1,6 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 
-import { createReducer } from '../Helpers';
+import { createReducer } from '@reduxjs/toolkit';
 
 const INITIAL_STATE = {
   currencies: [],
