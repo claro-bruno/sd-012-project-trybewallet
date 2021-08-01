8b6893b2b6f348d68001b47b9a57d3025118df12
camila-mp <ca.malvp@gmail.com>
2021-08-01 16:55:47 -0300

    cria header no componente Wallet e adiciona css

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import './wallet.css';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail } = this.props;
+
+    return (
+      <div id="wallet-header">
+        <header>
+          <h1>TrybeWallet</h1>
+          <h3 data-testid="email-field">
+            Email:&nbsp;
+            {userEmail}
+          </h3>
+          <h3 data-testid="total-field">
+            Despesa total: R$ 0
+          </h3>
+          <h3 data-testid="header-currency-field">BRL</h3>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Wallet.propTypes = {
+  userEmail: propTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Wallet);
