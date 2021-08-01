254ae9f5baad5aa5dfdd40c3bc30eb5ac148096a
Heder Waine <hederw@hotmail.com>
2021-08-01 12:23:31 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input
+          data-testid="email-input"
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          placeholder="Senha"
+        />
+        <button type="button">Entrar</button>
+      </form>
+    );
   }
 }
 
