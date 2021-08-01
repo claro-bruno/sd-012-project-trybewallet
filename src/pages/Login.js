82c1592552f808184244eb6e07fc637e4c8852b3
Eduardo Linhares <modsgtasagame@gmail.com>
2021-08-01 16:40:38 -0300

    Resolve os requisitos 2 e 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,23 +9,45 @@ class Login extends React.Component {
 
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      disabled: true,
     }
+  }
+
+  checkEmail = () => {
+    const { email, password } = this.state;
+
+    const VALID_EMAIL = /^[\w]+@([\w]+\.)+[\w]{2,4}$/gi;
+    const PASSWORD = password.length;
+
+    if (VALID_EMAIL.test(email) && PASSWORD >= 6) {
+      return this.setState({
+        disabled: false,
+      })
+    }
+
+    return this.setState({
+      disabled: true,
+    })
   }
 
   handleChange = ({ target: { name, value } }) => {
     this.setState({
       [name]: value
+    }, () => {
+      this.checkEmail();
     });
   }
 
   handleClick = () => {
-    const { Logar } = this.props;
+    const { Logar, history } = this.props;
     const { email } = this.state;
     Logar(email);
+    history.push('/carteira');
   }
 
   render() {
+    const { disabled } = this.state;
     return (
       <>
         <input
@@ -40,7 +62,7 @@ class Login extends React.Component {
           name="password"
           onChange={ this.handleChange }
         />
-        <button onClick={ this.handleClick }>Entrar</button>
+        <button onClick={ this.handleClick } disabled={ disabled }>Entrar</button>
       </>
     );
   }
