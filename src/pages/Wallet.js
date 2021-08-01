98d17de8f5b7d03769c53376278f16d48089086b
Rodrigo-R-Freitas <rodrigoaz8@gmail.com>
2021-08-01 11:54:36 -0300

    Requisito 6 - Form de cadastramento de despesa

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -11,6 +11,80 @@ class Wallet extends React.Component {
     };
   }
 
+  renderValueInput() {
+    return (
+      <label htmlFor="value-input">
+        Valor
+        <input
+          type="number"
+          id="value-input"
+          // value={ valueInputText }
+          // onChange={ this.handleChangeValue }
+        />
+      </label>
+    );
+  }
+
+  renderCurrencySelect() {
+    return (
+      <label htmlFor="currency-select" id="currency-label">
+        Moeda
+        <select
+          aria-labelledby="currency-label"
+          id="currency-select"
+        />
+      </label>
+    );
+  }
+
+  renderPaymentMethodSelect() {
+    return (
+      <label htmlFor="payment-method-select" id="payment-method-label">
+        Método de pagamento
+        <select
+          aria-labelledby="payment-method-label"
+          id="payment-method-select"
+        >
+          <option value="money">Dinheiro</option>
+          <option value="credit-card">Cartão de crédito</option>
+          <option value="debit-card">Cartão de débito</option>
+        </select>
+      </label>
+    );
+  }
+
+  renderTagSelect() {
+    return (
+      <label htmlFor="tag-select" id="tag-label">
+        Tag
+        <select
+          aria-labelledby="tag-label"
+          id="tag-select"
+        >
+          <option value="feeding">Alimentação</option>
+          <option value="leisure">Lazer</option>
+          <option value="work">Trabalho</option>
+          <option value="transport">Transporte</option>
+          <option value="health">Saúde</option>
+        </select>
+      </label>
+    );
+  }
+
+  renderDescriptionInput() {
+    return (
+      <label htmlFor="description-input">
+        Descrição
+        <input
+          type="text"
+          id="description-input"
+          // value={ descriptionInputText }
+          // onChange={ this.handleChangeDescription }
+        />
+      </label>
+    );
+  }
+
   render() {
     const { userEmail } = this.props;
     const { despesaTotal, selectedCurrency } = this.state;
@@ -25,9 +99,13 @@ class Wallet extends React.Component {
           </p>
         </header>
         <div>
-          <section>
-            TrybeWallet
-          </section>
+          <form>
+            { this.renderValueInput() }
+            { this.renderCurrencySelect() }
+            { this.renderPaymentMethodSelect() }
+            { this.renderTagSelect() }
+            { this.renderDescriptionInput() }
+          </form>
         </div>
       </main>
     );
