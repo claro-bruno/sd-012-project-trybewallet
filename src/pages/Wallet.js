8ac11ce24ed9064b3fee6c92a6337dfc113b7d6f
Thiago Prado <thiagopc_dev@hotmail.com>
2021-08-01 20:23:59 -0300

    Desafio 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
+import Input from '../components/Input';
+import Select from '../components/Select';
 
 class Wallet extends React.Component {
   render() {
@@ -7,6 +9,44 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <div>Wallet</div>
+        <form>
+          <Input
+            type="text"
+            label="Valor"
+            name="Valor"
+            id="Valor"
+          />
+          <Input
+            type="text"
+            label="Descrição"
+            name="Descrição"
+            id="Descrição"
+          />
+          <Select
+            label="Moeda"
+            name="Moeda"
+            id="Moeda"
+            defaultOption=""
+            defaultValue=""
+            options={ ['op1', 'op2', 'op3'] }
+          />
+          <Select
+            label="Método de pagamento"
+            name="Método de pagamento"
+            id="Método de pagamento"
+            defaultOption="Dinheiro"
+            defaultValue="Dinheiro"
+            options={ ['Cartão de crédito', 'Cartão de débito'] }
+          />
+          <Select
+            label="Tag"
+            name="Tag"
+            id="Tag"
+            defaultOption="Alimentação"
+            defaultValue="Alimentação"
+            options={ ['Lazer', 'Trabalho', 'Transporte', 'Saúde'] }
+          />
+        </form>
       </div>
     );
   }
