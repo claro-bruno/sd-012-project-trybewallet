5bbb96b872520281983795d980a2196c63f47aa4
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2021-08-01 16:10:17 -0300

    requisito 2 e inicia o 3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
-// Coloque aqui suas actions
+export const GET_EMAIL = 'GET_EMAIL';
+
+export function actionGetEmail(emailInput) {
+  return {
+    type: GET_EMAIL,
+    email: emailInput,
+  };
+}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { actionGetEmail } from '../actions/index';
 
 class Login extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
     this.state = {
       email: '',
       password: '',
+      button: true,
+
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validEntries = this.validEntries.bind(this);
+    this.handleSubmitEmail = this.handleSubmitEmail(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value },
+      () => this.validEntries());
+  }
+
+  // Essa parte do código retirei do repositória da colega Cristina Pineda
+
+  validEntries() {
+    const { email, password } = this.state;
+    const minLength = 6;
+    if (email.includes('@') && email.includes('.com') && password.length >= minLength) {
+      this.setState({
+        button: false,
+      });
+    } else {
+      this.setState({
+        button: true,
+      });
+    }
+  }
+
+  handleSubmitEmail() {
+    const { email } = this.state;
+    const { getEmailStore } = this.props;
+    getEmailStore(email);
   }
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, button } = this.state;
     return (
       <section>
         <h1>Login</h1>
@@ -23,6 +62,7 @@ class Login extends React.Component {
               value={ email }
               placeholder="Email"
               data-testid="email-input"
+              onChange={ this.handleChange }
             />
           </label>
           <label htmlFor="password">
@@ -33,17 +73,31 @@ class Login extends React.Component {
               value={ password }
               placeholder="Password"
               data-testid="password-input"
+              minLength="6"
+              onChange={ this.handleChange }
             />
           </label>
-          <button
-            type="button"
-          >
-            Entrar
-          </button>
+          <Link to="/carteira">
+            <button
+              id="button"
+              type="button"
+              disabled={ button }
+              onClick={ this.handleSubmitEmail }
+            >
+              Entrar
+            </button>
+          </Link>
         </form>
       </section>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  getEmailStore: PropTypes.func,
+}.isRequired;
+
+const mapDispatchToProps = (dispatch) => ({
+  getEmailStore: (emailInput) => dispatch(actionGetEmail(emailInput)),
+});
+export default connect(null, mapDispatchToProps)(Login);
