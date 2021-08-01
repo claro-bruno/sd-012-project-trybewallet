ab3ff4dd06a5ab504ef80cf0a5cf550331ed950c
Djaniza Vasques <djanizav@gmail.com>
2021-07-31 21:45:14 -0300

    Corrige Route em App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route to="/" component={ Login } />
-        <Route to="/carteira" component={ Wallet } />
+        <Route path="/carteira" component={ Wallet } />
+        <Route path="/" component={ Login } />
       </Switch>
     );
   }
