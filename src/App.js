f4e0a3723f9c3276394f2e2a000e519dfb0aa787
Renan Ferreira <renansantiago2013@hotmail.com>
2021-08-01 19:41:50 -0300

    Resolve o requisito 5 - Header wallet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
       </Switch>
     </div>
   );
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import './wallet.css';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+
+    return (
+      <div>
+        <header>
+          <div className="logo">
+            TrybeWallet
+          </div>
+          <div className="user-info">
+            <p data-testid="email-field" className="user-email">
+              {`Email: ${email}`}
+            </p>
+            <p data-testid="total-field">
+              Despesa Total: 0,00
+            </p>
+            <span data-testid="header-currency-field">BRL</span>
+          </div>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Wallet);
