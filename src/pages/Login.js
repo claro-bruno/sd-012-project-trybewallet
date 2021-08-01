9498b4a5af19c441fefb8d3911d0a68f0c350531
RenanMiyashita <renanmiyashita@gmail.com>
2021-08-01 00:32:28 -0300

    R2 ( Realiza as verificações em email, senha e botão ) concluido

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleValidate = this.handleValidate.bind(this);
+  }
+
+  componentDidUpdate() {
+    this.handleValidate();
+  }
+
+  handleValidate() {
+    const { email, password, disabled } = this.state;
+    const numbers = 6;
+    if (email.split('').includes('@')
+      && email.split('.').includes('com')
+      && password.length >= numbers
+      && disabled) {
+      this.setState({ disabled: false });
+    }
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { email, password, disabled } = this.state;
     return (
       <div>
         <label htmlFor="email">
-          <input type="email" name="email" data-testid="email-input" />
+          <input
+            type="email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+            data-testid="email-input"
+            placeholder="email@email.com"
+          />
         </label>
+
         <label htmlFor="password">
-          <input type="password" name="password" data-testid="password-input" />
+          <input
+            type="password"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+            data-testid="password-input"
+            placeholder="Senha"
+          />
         </label>
-        <button type="button">Entrar</button>
+
+        <button
+          type="button"
+          disabled={ disabled }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
 }
-
 export default Login;
