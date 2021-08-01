3ce083b19aeb46f8344f6fd7e0ee97bf581009e2
camila-mp <ca.malvp@gmail.com>
2021-08-01 18:16:19 -0300

    implementa formulario de despesa em Wallet (requisito 6) e adiciona css

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,11 +4,50 @@ import propTypes from 'prop-types';
 import './wallet.css';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.renderMetPag = this.renderMetPag.bind(this);
+    this.renderTag = this.renderTag.bind(this);
+
+    this.state = {
+
+    };
+  }
+
+  renderMetPag() {
+    return (
+      <label htmlFor="met-pagamento">
+        Método de pagamento
+        <select type="text" id="met-pagamento">
+          <option>Dinheiro</option>
+          <option>Cartão de crédito</option>
+          <option>Cartão de débito</option>
+        </select>
+      </label>
+    );
+  }
+
+  renderTag() {
+    return (
+      <label htmlFor="tag">
+        Tag
+        <select type="text" id="tag">
+          <option>Alimentação</option>
+          <option>Lazer</option>
+          <option>Trabalho</option>
+          <option>Transporte</option>
+          <option>Saúde</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     const { userEmail } = this.props;
 
     return (
-      <div className="wallet-header">
+      <div>
         <header>
           <h1>TrybeWallet</h1>
           <h3 data-testid="email-field">
@@ -20,6 +59,24 @@ class Wallet extends React.Component {
           </h3>
           <h3 data-testid="header-currency-field">BRL</h3>
         </header>
+        <form className="wallet-form">
+          <label htmlFor="valor">
+            Valor
+            <input type="text" id="valor" />
+          </label>
+          <label htmlFor="descricao">
+            Descrição
+            <input type="text" id="descricao" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select type="text" id="moeda">
+              <option>moeda1</option>
+            </select>
+          </label>
+          { this.renderTag() }
+          { this.renderMetPag() }
+        </form>
       </div>
     );
   }
