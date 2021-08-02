6ccf48e866fb1d0dcaeb2dab703df0a6684e789e
thales-daniel <thalesdaniel2013@gmail.com>
2021-08-02 19:09:42 -0300

    feito etapa 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,78 @@
 import React from 'react';
 import '../App.css';
 
+const validEmail = /^[a-z0-9](\.|-|_|[a-z]|\d)+@([a-z]|\d)+\.[a-z]{2,3}(\.[a-z]{2})?$/;
+
 class Login extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      // inputEmail: '',
-      // inputPassword: '',
+      Email: '',
+      Password: '',
+      login: false,
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.validator = this.validator.bind(this);
+  }
+
+  validator() {
+    const { Email, Password } = this.state;
+    const validPassword = 6;
+    const testEmail = validEmail.test(Email);
+    const testPassword = Password.length >= validPassword;
+    if (testEmail && testPassword) {
+      this.setState({
+        login: true,
+      });
+    }
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    }, () => this.validator());
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { login } = this.state;
+    if (login) {
+      console.log('funcionou');
+    }
   }
 
   render() {
+    const { Email, Password, login } = this.state;
     return (
       <div className="container">
         <div className="login-container">
-          <input className="login" type="text" data-testid="email-input" />
-          <input className="login" type="text" data-testid="password-input" />
-          <button className="btn" type="submit">Entrar</button>
+          <input
+            onChange={ this.handleChange }
+            value={ Email }
+            name="Email"
+            className="login"
+            type="text"
+            data-testid="email-input"
+          />
+          <input
+            onChange={ this.handleChange }
+            value={ Password }
+            name="Password"
+            className="login"
+            type="text"
+            data-testid="password-input"
+          />
+          <button
+            text="Entrar"
+            disabled={ !login }
+            className="btn"
+            type="submit"
+            onClick={ this.handleSubmit }
+          >
+            Entrar
+          </button>
         </div>
       </div>);
   }
