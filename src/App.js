321ad5a3b2d0155de4fef7a2f0c8afb77802d57d
thales-daniel <thalesdaniel2013@gmail.com>
2021-08-02 12:23:14 -0300

    feito etapa 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import '../App.css';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // inputEmail: '',
+      // inputPassword: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="container">
+        <div className="login-container">
+          <input className="login" type="text" data-testid="email-input" />
+          <input className="login" type="text" data-testid="password-input" />
+          <button className="btn" type="submit">Entrar</button>
+        </div>
+      </div>);
   }
 }
 
