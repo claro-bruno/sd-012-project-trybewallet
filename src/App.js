70b1445897a576e8e49f24cb85b537021fb24a59
Adriana Biberg <adribiberg@hotmail.com>
2021-08-02 17:56:41 -0300

    Cumpre requisito 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route path="/" component={ Login } />
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/carteira" component={ Wallet } />
         </Switch>
       </div>
     );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      btnSubmit: true,
+    };
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangeBtnPassword = this.handleChangeBtnPassword.bind(this);
+  }
+
+  handleChangeEmail(event) {
+    const emailValue = event.target.value;
+    console.log(emailValue);
+    this.setState({ email: emailValue });
+  }
+
+  handleChangeBtnPassword(event) {
+    const { email } = this.state;
+    const validPass = 6;
+    const password = event.target.value;
+    const validEmail = /\S+@\S+\.\S+/;
+    if (validEmail.test(email) && password.length >= validPass) {
+      this.setState({ btnSubmit: false });
+    } else {
+      this.setState({ btnSubmit: true });
+    }
+  }
+
   render() {
+    const { btnSubmit } = this.state;
+    const { history } = this.props;
     return (
-      <div>
+      <form>
         <div>Login</div>
-        <input type="email" placeholder="Insira seu e-mail" data-testid="email-input" />
+        <input
+          type="email"
+          placeholder="Insira seu e-mail"
+          data-testid="email-input"
+          onChange={ this.handleChangeEmail }
+        />
         <input
           type="password"
           placeholder="Insira sua senha"
           data-testid="password-input"
+          onChange={ this.handleChangeBtnPassword }
         />
-        <button type="button">Entrar</button>
-      </div>
+        <button
+          type="button"
+          disabled={ btnSubmit }
+          onClick={ () => history.push('/carteira') }
+        >
+          Entrar
+        </button>
+      </form>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
