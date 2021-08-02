a565c668e60dc9f83c22cfe3ff89e16a1b3848e1
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-08-02 15:29:46 -0300

    Adiciona requisito 4

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
       </Switch>
     );
   }
