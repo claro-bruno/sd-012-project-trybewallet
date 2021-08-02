c7ae9e68e75b2fa2ef739a5029b0953f1485ae4d
tiagoornelas <advtiagoornelas@gmail.com>
2021-08-02 16:30:53 -0300

    [LINT] Resolve lint no arquivo index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
-import { BrowserRouter } from 'react-router-dom';
 
 // prettier-ignore
 ReactDOM.render(
