dc80e063cb75c3854a75828fb093473ef3b35c0e
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-08-02 14:45:54 -0300

    Requisito 1 mais estrutura Redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-// Coloque aqui suas actions
+export const ADD_LOGIN = 'ADD_LOGIN';
+export const CHANGE_WALLET = 'CHANGE_WALLET';
+
+export const addLogin = (login) => ({
+  type: ADD_LOGIN,
+  payload: login,
+});
+
+export const changeWallet = (change) => ({
+  type: CHANGE_WALLET,
+  payload: change,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="email-input">
+          E-Mail
+          <input type="text" data-testid="email-input" />
+        </label>
+        <label htmlFor="password-input">
+          Senha
+          <input type="password" data-testid="password-input" />
+        </label>
+        <button type="button">
+          Entrar
+        </button>
+      </div>);
   }
 }
 
