c05c34ea6e7517005ef251ec449b1161481a6cf8
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-08-01 23:51:08 -0300

    Implementa roteamento

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import ProtectedRoute from './Components/ProtectedRoute';
+// import { PropTypes } from 'prop-types';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Wallet from './pages/Wallet';
 
 class App extends Component {
   render() {
-    return <div>Hello, TrybeWallet!</div>;
+    return (
+      <main>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={ Login }
+          />
+          <ProtectedRoute
+            path="/carteira"
+            redirectRoute="/"
+            component={ Wallet }
+          />
+          <Route
+            component={ NotFound }
+          />
+        </Switch>
+      </main>
+    );
   }
 }
+
+// App.propTypes = {
+//   isLoggedIn: PropTypes.bool.isRequired,
+// };
 
 export default App;
