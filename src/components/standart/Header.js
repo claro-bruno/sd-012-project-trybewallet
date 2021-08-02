1315c8d9d9b37221bc396d00325c207bf3980e88
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-08-02 17:33:25 -0300

    implementar function para incluir o valor total da compra no component Header e conseguinte conclusao do requisito_08

--- src/components/standart/Header.js
+++ src/components/standart/Header.js
@@ -3,15 +3,53 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.totalExpenses = this.totalExpenses.bind(this);
+  }
+
+  totalExpenses() {
+    const {
+      props: { expenses },
+    } = this;
+
+    const GrandTotal = expenses.reduce((accumuladorOne, expense) => {
+      const { exchangeRates, currency, value } = expense;
+
+      const filterCurrency = Object.values(exchangeRates)
+        .filter((coin) => {
+          const { code, codein } = coin;
+          return code === currency && codein === 'BRL';
+        });
+
+      const currencyConverter = filterCurrency.reduce((accumulatorTwo, coin) => {
+        const { ask } = coin;
+        accumulatorTwo += (+ask * +value);
+        return accumulatorTwo;
+      }, 0);
+
+      accumuladorOne += currencyConverter;
+
+      return accumuladorOne;
+    }, 0);
+
+    const roundingValues = (Math.round((GrandTotal + Number.EPSILON) * 100) / 100);
+    // fonte: https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
+
+    return roundingValues;
+  }
+
   render() {
     const {
       props: { emailStore },
+      totalExpenses,
     } = this;
 
     return (
       <ul>
         <li data-testid="email-field">{`Email: ${emailStore}`}</li>
-        <li data-testid="total-field">{ `Despesa Total: ${0}` }</li>
+        <li data-testid="total-field">{ `Despesa Total: R ${totalExpenses()}` }</li>
         <li data-testid="header-currency-field">Cambio: BRL</li>
       </ul>
     );
@@ -20,11 +58,19 @@ class Header extends React.Component {
 
 const mapStateToProps = (state) => ({
   emailStore: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
-const { string } = PropTypes;
+const { string, arrayOf, objectOf, oneOfType, number, object } = PropTypes;
 Header.propTypes = {
   emailStore: string.isRequired,
+  expenses: arrayOf(
+    objectOf(oneOfType([
+      string,
+      number,
+      object,
+    ])),
+  ).isRequired,
 };
 
 export default connect(mapStateToProps, null)(Header);
