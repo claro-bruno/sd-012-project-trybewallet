c488fd6ae99666ad3ad1f9c00629e459e58cf637
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-02 11:47:19 -0300

    implementa inputs em page login, implementa rota em App.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="email">
+          E-mail
+          <input id="email" type="email" name="email" data-testid="email-input" />
+        </label>
+        <label htmlFor="pass">
+          Senha
+          <input id="pass" type="password" name="pass" data-testid="password-input" />
+        </label>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import rootReduce from '../reducers';
 
 const store = createStore(rootReduce);
 
