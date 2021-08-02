01ff9dcaf088062d0e396bc54436e73415a4b521
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-08-02 14:44:39 -0300

    Implementa requesito 5, mas falta pegar informaçes da Store!

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <header>
+        <div>
+          Email:
+          <span data-testid="email-field">
+            email@.com
+          </span>
+        </div>
+        <div>
+          Despesa Total:
+          <span data-testid="total-field">
+            0
+          </span>
+          <span data-testid="header-currency-field">
+            BRL
+          </span>
+        </div>
+      </header>
+    );
   }
 }
 
