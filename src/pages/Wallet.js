2decf2dbfd3ab6ada450b6e488de7cf5f327dffd
Matheus Proença <mproenca.marketing@gmail.com>
2021-08-02 16:04:58 -0300

    Adiciona header - Req:05

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { total } = this.state;
+    const { userEmail } = this.props;
+
+    return (
+      <div>
+        <header>
+          <p
+            data-testid="email-field"
+          >
+            {`Email: ${userEmail}`}
+          </p>
+          <p
+            data-testid="total-field"
+          >
+            {`Despesa Total: ${total}`}
+          </p>
+          <p
+            data-testid="header-currency-field"
+          >
+            BRL
+          </p>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
