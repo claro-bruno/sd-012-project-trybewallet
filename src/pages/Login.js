ab158ff6d2dacaa6c604d153136d97fa1d26c6e9
MaxAlves0035 <maxalves2@hotmail.com>
2021-08-02 15:22:12 -0300

    Cria o campo de email e senha no component Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,52 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { id, value } = target;
+    this.setState({
+      [id]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <section>
+        <div>
+          <label htmlFor="email" data-testid="email-input">
+            Email:
+            <input
+              type="text"
+              id="email"
+              value={ email }
+              onChange={ this.handleChange }
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="password" data-testid="password-input">
+            Senha:
+            <input
+              type="password"
+              id="password"
+              value={ password }
+              onChange={ this.handleChange }
+            />
+          </label>
+        </div>
+        <button type="submit">Entrar</button>
+      </section>
+    );
   }
 }
 
