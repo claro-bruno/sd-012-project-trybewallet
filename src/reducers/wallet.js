44dc0523d6ceb1c4e0dad65240ee5081d196354d
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-02 12:35:04 -0300

    implementa modelo base de reducer em wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,16 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INICIA_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = INICIA_STATE, action) => {
+  switch (action.type) {
+  case 'teste':
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default wallet;
