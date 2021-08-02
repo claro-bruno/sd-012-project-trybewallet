706cce0be006b46651700aac0b05773d507c57f4
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-02 12:37:27 -0300

    implementa modelo reducer user

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INICIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INICIAL_STATE, action) => {
+  switch (action.type) {
+  case 'teste':
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default user;
