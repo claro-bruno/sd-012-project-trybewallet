b2cacd8611d1a1255a11fbf26ec664b3ae44f515
lucasnahas <lucasahas@gmail.com>
2021-08-02 15:54:22 -0300

    Faz forms de dispesa

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Form from '../components/Form';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { user } = this.props;
+    return (
+      <div>
+        <header>
+          <span data-testid="email-field">{ user.email }</span>
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </header>
+        <Form />
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => (
+  {
+    user: state.user,
+  }
+);
+export default connect(mapStateToProps, null)(Wallet);
+
+Wallet.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string,
+  }).isRequired,
+};
