8e6d87987581be02af29a61833ec1aaf56b68ee1
Rafael Cardoso <rafael.vimur@gmail.com>
2021-08-02 16:37:33 -0300

    adiona requisito 9, criação da tabela

---
+++ src/components/Table.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Table extends Component {
+  render() {
+    const { expenses } = this.props;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((item) => {
+            const cambio = item.exchangeRates[item.currency].ask;
+            const currencyName = item.exchangeRates[item.currency].name;
+            return (
+              <tr key={ item.id }>
+                <td>{item.description}</td>
+                <td>{item.tag}</td>
+                <td>{item.method}</td>
+                <td>{item.value}</td>
+                <td>{currencyName}</td>
+                <td>{Number(cambio).toFixed(2)}</td>
+                <td>{(cambio * item.value).toFixed(2)}</td>
+                <td>Real</td>
+                <td>Excluir</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Table);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Forms from '../components/Forms';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <Forms />
+        <Table />
       </div>
     );
   }
