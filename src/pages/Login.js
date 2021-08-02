44effd2a105d3ff04187e7932ef5838f1e735597
Roberval Filho <rslfilho@gmail.com>
2021-08-02 14:48:11 -0300

    Component Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,80 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.renderEmailInput = this.renderEmailInput.bind(this);
+    this.renderPasswordInput = this.renderPasswordInput.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  renderEmailInput() {
+    const { email } = this.state;
+
+    return (
+      <label htmlFor="email">
+        Email:
+        <input
+          id="email"
+          name="email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  renderPasswordInput() {
+    const { password } = this.state;
+
+    return (
+      <label htmlFor="password">
+        Senha:
+        <input
+          id="password"
+          name="password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  renderEnterButton() {
+    return (
+      <button
+        type="submit"
+      >
+        Entrar
+      </button>
+    );
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <h2>Login</h2>
+        { this.renderEmailInput() }
+        { this.renderPasswordInput() }
+        { this.renderEnterButton() }
+      </form>
+    );
   }
 }
 
