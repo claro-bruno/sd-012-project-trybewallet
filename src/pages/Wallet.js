73fb9a08adc298cc7bfbf964c761d7dd14cfd734
Rafael Primon <rafaelfernandesprimon@gmail.com>
2021-08-02 17:08:12 -0300

    req 6 - cria formulario e passa rota no wallet.js

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Forms from '../components/Formulario';
 
 class Wallet extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class Wallet extends React.Component {
       <div>
         <div>TrybeWallet</div>
         <Header />
+        <Forms />
       </div>
     );
   }
