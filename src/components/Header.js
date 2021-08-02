020496aa6142c3a5d9511469c97f481acd4fc83d
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-08-02 15:09:36 -0300

    feat total value in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,16 +9,29 @@ class Header extends React.Component {
     this.state = {
       total: 0,
     };
+    this.getTotalValue = this.getTotalValue.bind(this);
+  }
+
+  getTotalValue() {
+    const { expenses } = this.props;
+    
+
+    const totalValue = Math.round(expenses.reduce((acc, expense) => {
+      const { value, currency, exchangeRates } = expense;
+      const { ask } = exchangeRates[currency];
+      return acc += parseFloat(value) * parseFloat(ask);
+    }, 0) * 100)/ 100;
+    return totalValue;
   }
 
   render() {
     const { userEmail } = this.props;
-    const { total } = this.state;
+    const { getTotalValue } = this;
     return (
       <header>
         <h1>Weballet</h1>
         <p data-testid="email-field">{ userEmail }</p>
-        <p data-testid="total-field">{ total }</p>
+        <p data-testid="total-field">{ getTotalValue() }</p>
         <p data-testid="header-currency-field">BRL</p>
       </header>
     );
@@ -27,6 +40,7 @@ class Header extends React.Component {
 
 const mapStateToProps = (state) => ({
   userEmail: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 Header.propTypes = {
