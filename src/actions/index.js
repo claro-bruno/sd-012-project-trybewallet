05d8a5a2de286a969d447f7cea1d09ad33209bac
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-08-02 10:16:21 -0300

    Implementa requisição a API

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,3 +9,17 @@ export const addExpense = (expense) => ({
   type: 'ADD_EXPENSE',
   expense,
 });
+
+const URL = 'https://economia.awesomeapi.com.br/json/all';
+
+const sendRequest = () => ({ type: 'SEND_REQUEST' });
+const gotResponse = (json) => ({ type: 'GOT_RESPONSE', json });
+const gotError = (error) => ({ type: 'GOT_ERROR', error });
+
+export const fetchAPI = () => (dispatch) => {
+  dispatch(sendRequest());
+  fetch(URL)
+    .then((response) => response.json())
+    .then((json) => dispatch(gotResponse(json)))
+    .catch((error) => dispatch(gotError(error)));
+};
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,17 @@
 /* eslint-disable max-lines-per-function */
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../Components/Header';
 import ExpenseInput from '../Components/ExpenseInput';
+import { fetchAPI } from '../actions';
 
 class Wallet extends React.Component {
+  componentDidMount() {
+    const { dispatchCurrenciesToStore } = this.props;
+    dispatchCurrenciesToStore();
+  }
+
   render() {
     return (
       <>
@@ -14,4 +22,12 @@ class Wallet extends React.Component {
   }
 }
 
-export default Wallet;
+const mapDispatchToProps = (dispatch) => ({
+  dispatchCurrenciesToStore: () => dispatch(fetchAPI()),
+});
+
+export default connect(null, mapDispatchToProps)(Wallet);
+
+Wallet.propTypes = {
+  dispatchCurrenciesToStore: PropTypes.func.isRequired,
+};
