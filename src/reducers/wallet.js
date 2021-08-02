618bc034e882a26c83a847d58522cf6b95ecc76a
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-08-02 10:13:32 -0300

    Implementa id de despesa | Lida com requisição a API

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,14 +1,37 @@
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
+  expenseIdCounter: 0,
+  currenciesAreLoading: false,
+  apiError: [],
 };
 
 const wallet = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case 'ADD_EXPENSE':
     return {
-      currencies: [...state.currencies],
-      expenses: [...state.expenses, action.expense],
+      ...state,
+      expenses: [...state.expenses, { ...action.expense, id: state.expenseIdCounter }],
+      expenseIdCounter: state.expenseIdCounter + 1,
+    };
+  case 'SEND_REQUEST':
+    return {
+      ...state,
+      currenciesAreLoading: true,
+    };
+  case 'GOT_RESPONSE':
+    return {
+      ...state,
+      currencies: [...state.currencies,
+        ...Object.values(action.json)
+          .filter((value) => value.codein !== 'BRLT')],
+      currenciesAreLoading: false,
+    };
+  case 'GOT_ERROR':
+    return {
+      ...state,
+      apiError: [...state.apiError, action.error],
+      currenciesAreLoading: false,
     };
   default: return state;
   }
