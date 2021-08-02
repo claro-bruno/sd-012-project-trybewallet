d68e3525b07e8525bf28fb50367ac19c10c34044
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-08-02 12:21:52 -0300

    R8| Adiciona total de despesas em Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,14 +5,17 @@ import PropTypes from 'prop-types';
 
 class Header extends React.Component {
   render() {
-    const { email } = this.props;
+    const { email, expenses } = this.props;
     return (
       <header>
         <span data-testid="email-field">
           {email}
         </span>
         <span data-testid="total-field">
-          0
+          {expenses.reduce((acc, curr) => acc
+            + parseFloat(curr.value)
+            * parseFloat(curr.exchangeRates[curr.currency].ask),
+          0)}
         </span>
         <span data-testid="header-currency-field">
           BRL
@@ -24,10 +27,12 @@ class Header extends React.Component {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 export default connect(mapStateToProps)(Header);
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.shape()).isRequired,
 };
