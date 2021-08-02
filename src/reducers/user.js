af59c996bd783b5c61315c26e20844ab805d8605
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-08-01 23:48:23 -0300

    Adiciona e combina reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,14 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+  isLoggedIn: false,
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'LOG_IN':
+    return { ...state, isLoggedIn: true, email: action.email };
+  default: return state;
+  }
+};
+
+export default user;
