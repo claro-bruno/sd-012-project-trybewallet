a38f6a1e0b0674c0a4c22fc71d588697a2b254c5
Martin <escorpmartin97@gmail.com>
2021-08-02 15:01:08 -0300

    [Estava colocando o BrowserRouter duas vezes e nao passavam os requisitos nos testes]

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route exact path="/carteira" component={ Wallet } />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
+      </Switch>
     );
   }
 }
