e94da22de12f50741d247de82c2ac3291ce69d38
Augusto <augustocm2002@yahoo.com.br>
2021-08-02 19:23:11 -0300

    Fazendo requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      Hello, TrybeWallet!
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <div>Login</div>
+        <input data-testid="email-input" type="email" placeholder="'alguem@alguem.com'" />
+        <input
+          data-testid="password-input"
+          type="password"
+          placeholder="Senha:"
+          minLength="6"
+        />
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
