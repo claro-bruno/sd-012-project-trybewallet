b22688d4c778c953e399dae0d818b294d6367be3
Gabriel Viana <g.viana87@hotmail.com>
2021-08-02 10:14:03 -0300

    render inputs e button para login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,47 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      enable: false,
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, enable } = this.state;
+    return (
+      <main>
+        <section>
+          <input
+            data-testid="email-input"
+            type="text"
+            name="email"
+            value={ email }
+            placeholder="Email"
+            onChange={ this.handleChange }
+          />
+          <input
+            data-testid="password-input"
+            type="password"
+            name="password"
+            value={ password }
+            placeholder="Password"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            onClick={ this.handleSaveLogin }
+            disabled={ enable }
+          >
+            ENTRAR
+          </button>
+        </section>
+      </main>
+    );
   }
 }
 
