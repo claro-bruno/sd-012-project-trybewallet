7a553772b9e5faf5caa9795ed256cd30eb1950de
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-08-01 23:50:40 -0300

    Adiciona HOC ProtectedRoute

---
+++ src/Components/ProtectedRoute.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class ProtectedRoute extends React.Component {
+  render() {
+    const { isLoggedIn, component: Component, redirectRoute, ...rest } = this.props;
+    return (
+      <Route
+        { ...rest }
+        render={ (props) => (
+          isLoggedIn
+            ? <Component { ...props } />
+            : <Redirect to={ redirectRoute } />
+        ) }
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isLoggedIn: state.user.isLoggedIn,
+});
+
+export default connect(mapStateToProps)(ProtectedRoute);
+
+ProtectedRoute.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  component: PropTypes.func.isRequired,
+  redirectRoute: PropTypes.string.isRequired,
+};
