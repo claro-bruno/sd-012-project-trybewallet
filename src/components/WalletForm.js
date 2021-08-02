15824d964a25f94bb3a85bdd17c25ac5f1014dff
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-08-02 19:25:39 -0300

    adiciona o form na carteira com todos os valores pedidos para teste

---
+++ src/components/WalletForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class WalletForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="value">
+          Valor:
+          <input type="text" id="value" />
+        </label>
+        <label htmlFor="description">
+          Descrição:
+          <input type="text" id="description" />
+        </label>
+        <label htmlFor="currency">
+          Moeda:
+          <select id="currency">
+            <option> </option>
+          </select>
+        </label>
+        <label htmlFor="payment">
+          Método de pagamento:
+          <select id="payment">
+            <option>Dinheiro</option>
+            <option>Cartão de crédito</option>
+            <option>Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="type">
+          Tag:
+          <select id="type">
+            <option>Alimentação</option>
+            <option>Lazer</option>
+            <option>Trabalho</option>
+            <option>Transporte</option>
+            <option>Saúde</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default WalletForm;
