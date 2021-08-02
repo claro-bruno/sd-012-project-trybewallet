d1107f65e7ae7d8629e949fbd16d171197c493b1
jmlinger <jmlinger10@gmail.com>
2021-08-02 19:48:41 -0300

    Resolve requisito 5: foi criado um header que exibe o email do usuario e as despesas totais

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import './App.css';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      totalExpense: 0,
+      currency: 'BRL',
+    };
+  }
+
   render() {
+    const { email } = this.props;
+    const { totalExpense, currency } = this.state;
     return (
-      <div>
-        TrybeWallet
-      </div>
+      <header className="wallet-header">
+        <h4 data-testid="email-field">
+          Email:
+          { email }
+        </h4>
+        <div className="expenses-container">
+          <h4 data-testid="total-field" className="totalExpense">
+            Despesa Total:
+            { totalExpense }
+          </h4>
+          <h4 data-testid="header-currency-field">
+            { currency }
+          </h4>
+        </div>
+      </header>
     );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
