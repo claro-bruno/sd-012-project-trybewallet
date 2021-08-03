cbf10eedca336111345edf6349a4b7f66927edc5
MaxAlves0035 <maxalves2@hotmail.com>
2021-08-03 12:23:25 -0300

    Requisito 05 - aprovado, obs Requisito 04 ja foi aprovado tb

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currencies: [],
+      expenses: [],
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <header>
+        <div>
+          <h3 data-testid="email-field">{ email }</h3>
+        </div>
+        <div>
+          <span data-testid="total-field">
+            0
+          </span>
+        </div>
+        <div>
+          <span data-testid="header-currency-field">
+            BRL
+          </span>
+        </div>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => (
+  { email: state.user.email }
+);
+
+export default connect(mapStateToProps)(Wallet);
