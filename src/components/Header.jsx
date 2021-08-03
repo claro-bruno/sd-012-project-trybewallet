eb7750523168db69ad5098ea9a842a3a1844d669
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-03 10:47:33 -0300

    implementa logica ao header conforme requisito 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends React.Component {
   render() {
+    const { expenses, email } = this.props;
     return (
       <header>
         <h1>MyWallet</h1>
         <div>
-          <p>email</p>
-          <p>custo</p>
+          <p data-testid="email-field">{`Email: ${email}`}</p>
+          <p data-testid="total-field">
+            {`Dispesas Total: 0 ${expenses}`}
+            <span data-testid="header-currency-field">BRL</span>
+          </p>
         </div>
       </header>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  expenses: state.wallet.expenses,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
