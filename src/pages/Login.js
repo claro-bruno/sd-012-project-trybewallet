39dba7134db4e64fa92a94a27f0dcca1975e8306
Gustavo-Mourao <gcmbh1@gmail.com>
2021-08-03 10:46:16 -0300

    Iniciando- Q1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="email-input">
+          insira seu e-mail:
+          <input
+            data-testid="email-input"
+            type="email"
+            placeholder="e-mail"
+          />
+        </label>
+        <label htmlFor="password-input">
+          insira sua senha:
+          <input
+            data-testid="password-input"
+            type="password"
+            placeholder="pasword"
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
