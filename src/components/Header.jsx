46e3b9b7b150a7dccf80d852fc16418ca082d16d
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-03 10:24:58 -0300

    cria o componente header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>MyWallet</h1>
+        <div>
+          <p>email</p>
+          <p>custo</p>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
