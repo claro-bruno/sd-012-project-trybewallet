ad849347114327a765942041502a6a6ff88717b3
MaxAlves0035 <maxalves2@hotmail.com>
2021-08-03 10:51:54 -0300

    Requisito 02 - aprovado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { userEmail, userPwd } from '../actions';
 
 class Login extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       email: '',
       password: '',
+      disable: true,
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmitUser = this.handleSubmitUser.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+    this.emailIsValid = this.emailIsValid.bind(this);
+  }
+
+  // https://ui.dev/validate-email-address-javascript/ fonte retirada.
+  emailIsValid(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  validateForm() {
+    const { email, password } = this.state;
+    const minPassword = 5;
+    if (this.emailIsValid(email) === true && password.length >= minPassword) {
+      return this.setState({ disable: false });
+    }
+
+    return this.setState({ disable: true });
   }
 
   handleChange({ target }) {
@@ -19,6 +39,7 @@ class Login extends React.Component {
     this.setState({
       [id]: value,
     });
+    this.validateForm();
   }
 
   handleSubmitUser() {
@@ -29,33 +50,44 @@ class Login extends React.Component {
   }
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, disable } = this.state;
     return (
-      <section>
+      <form onSubmit={ this.handleSubmitUser }>
         <div>
-          <label htmlFor="email" data-testid="email-input">
+          <label htmlFor="email">
             Email:
             <input
-              type="text"
+              type="email"
               id="email"
+              data-testid="email-input"
               value={ email }
               onChange={ this.handleChange }
             />
           </label>
         </div>
         <div>
-          <label htmlFor="password" data-testid="password-input">
+          <label htmlFor="password">
             Senha:
             <input
               type="password"
               id="password"
+              data-testid="password-input"
               value={ password }
               onChange={ this.handleChange }
             />
           </label>
         </div>
-        <button type="submit" onClick={ this.handleSubmitUser }>Entrar</button>
-      </section>
+        <div>
+          <Link to="/carteira">
+            <button
+              type="submit"
+              disabled={ disable }
+            >
+              Entrar
+            </button>
+          </Link>
+        </div>
+      </form>
     );
   }
 }
@@ -65,5 +97,10 @@ const mapDispatchToProps = (dispatch) => (
     submitEmail: (email) => dispatch(userEmail(email)),
     submitPwd: (pwd) => dispatch(userPwd(pwd)),
   });
+
+Login.propTypes = {
+  submitEmail: PropTypes.func.isRequired,
+  submitPwd: PropTypes.func.isRequired,
+};
 
 export default connect(null, mapDispatchToProps)(Login);
