8b3f3eba7b0a12d9d6730dc899296964501f0f7b
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-03 12:57:53 -0300

    implementa fecth das moedas e passa para o component

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,16 +1,53 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import ExpenseForm from '../components/ExpenseForm';
 import Header from '../components/Header';
+import { addCurrencies } from '../actions';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.fetchCurrency = this.fetchCurrency.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCurrency();
+  }
+
+  fetchCurrency() {
+    const { addCurrency } = this.props;
+    fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => response.json())
+      .then((response) => {
+        const currencies = Object.keys(response).filter((key) => key !== 'USDT');
+        addCurrency(currencies);
+      });
+  }
+
   render() {
+    const { currencies } = this.props;
     return (
       <div>
         <Header />
-        <ExpenseForm />
+        <ExpenseForm currencies={ currencies } />
       </div>
     );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  currencies: state.wallet.currencies,
+});
+
+const mapDispatchToProps = (dispatch) => (
+  { addCurrency: (currencies) => dispatch(addCurrencies(currencies)) }
+);
+
+Wallet.propTypes = {
+  addCurrency: PropTypes.func.isRequired,
+  currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
