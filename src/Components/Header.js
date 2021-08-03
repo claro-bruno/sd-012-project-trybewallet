44b6e12e11e61797bad84dc02936e1b02c6126ae
Gabriel Viana <g.viana87@hotmail.com>
2021-08-03 11:04:48 -0300

    comenta css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import logoPaturso from '../Img/logoPaturso.jpg';
-import './HeaderStyle.css';
+// import './HeaderStyle.css';
 
 class Header extends Component {
   render() {
