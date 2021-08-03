547cbcf58c1d82b42aa5489d052d166069b66e1f
Luís Seixas <luiseixas2@gmail.com>
2021-08-03 11:09:13 -0300

    Implementa o requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <div>Login</div>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            data-testid="email-input"
+            placeholder="Digite um email"
+          />
+        </label>
+        <label htmlFor="password">
+          Digite uma senha:
+          <input
+            type="password"
+            data-testid="password-input"
+            placeholder="Digite sua senha"
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </>
+    );
   }
 }
 
