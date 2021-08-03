e43dd7c43d6b86f4d196249566df4a29df23b5c0
Ana-Jacomassi <anajacomassip@gmail.com>
2021-08-03 03:06:09 -0300

    requisito 1 - formulario basico login e senha com botao submit - store, reducer

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <div>Login</div>
+        <label htmlFor="email-input">
+          Insira seu e-mail:
+          <input
+            data-testid="email-input"
+            type="email"
+            id="umail-id"
+            placeholder="E-mail"
+          />
+        </label>
+        <label htmlFor="password-input">
+          Insira sua senha:
+          <input
+            data-testid="password-input"
+            type="password"
+            id="password-id"
+            placeholder="Password"
+          />
+        </label>
+        <button type="submit" id="button-submit">Entrar</button>
+      </section>
+    );
   }
 }
 
