31b6731b01e0e6b4b0ba86dff6d5e32c38d6f451
Guilherme Moreno <guilherme.lmoreno23@gmail.com>
2021-08-03 16:10:49 -0300

    Requesito 1 - Criando input email e login usuario

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,32 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            // value={this.state.value}
+            // onChange={this.handleChange}
+            data-testid="email-input"
+            id="email"
+            name="email"
+            placeholder="Digite seu e-mail"
+          />
+
+          <input
+            type="text"
+            // value={this.state.value}
+            // onChange={this.handleChange}
+            data-testid="password-input"
+            id="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <input type="submit" value="Entrar" />
+        </form>
+      </div>
+    );
   }
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,16 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+const ESTADO_INICIAL = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+const reducer = (state = ESTADO_INICIAL) => state;
+
+export default reducer;
