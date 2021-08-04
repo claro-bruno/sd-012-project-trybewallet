0fb9b570d13ef8c2c99a99cbead7ed91a4387009
Eric Faria <ericdeofaria@gmail.com>
2021-08-04 11:11:43 -0300

    Requisito 06

---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+export default class Form extends Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="valor">
+          Valor:
+          <input type="number" html="valor" />
+        </label>
+        <label htmlFor="descricao">
+          Descrição:
+          <input type="text" html="descricao" />
+        </label>
+        <label htmlFor="moeda">
+          Moeda:
+          <select value="API" id="moeda">
+            <option value="API" id="moeda">API</option>
+          </select>
+        </label>
+        <label htmlFor="pagamento">
+          Método de pagamento:
+          <select value="dinheiro" id="pagamento">
+            <option value="dinheiro" id="pagamento">Dinheiro</option>
+            <option value="credito" id="pagamento">Cartão de crédito</option>
+            <option value="debito" id="pagamento">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          TAG:
+          <select value="alimentacao" id="tag">
+            <option value="alimentacap" id="tag">Alimentação</option>
+            <option value="lazer" id="tag">Lazer</option>
+            <option value="trabalho" id="tag">Trabalho</option>
+            <option value="transporte" id="tag">Transporte</option>
+            <option value="saude" id="tag">Saúde</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import Form from '../components/Form';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <Form />
+      </>
     );
   }
 }
