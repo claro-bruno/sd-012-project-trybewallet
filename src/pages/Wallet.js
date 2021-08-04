2c9a0251755643004bc36d4dbf5c25a639302083
Eric Faria <ericdeofaria@gmail.com>
2021-08-04 10:34:45 -0300

    Correções de Linter

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Header from '../components/Header';
+
 class Wallet extends React.Component {
   render() {
     return (
-      <>
-        <Header />
-      </>
+      <Header />
     );
   }
 }
