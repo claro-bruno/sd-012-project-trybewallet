2c5e9a8291575a2c60e857be8c3e837720d64cd3
Thalles Carneiro <thallescarneiro@gmail.com>
2021-08-04 10:59:38 -0300

    Acrescenta comentario com o link de onde consultei a parte de validaçao do forms

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,7 @@
+/**
+ * Consultei o repositório de um projeto em conjunto com o Márcio Daniel, que fez essa parte da validação.
+ * https://github.com/grupo-de-estudos-T08/trivia-programming/blob/master/frontend/src/utils/index.js
+ */
 export const verifyEmail = (email) => {
   const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
   return regex.test(email);
