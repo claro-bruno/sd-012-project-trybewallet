1006f4fc4eff85ac725a47afde916bb31dd64ea8
Wadson Almeida <wadson.dev92@gmail.com>
2021-08-04 23:02:15 -0300

    Requisito 2, validaçao de usuario

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,44 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-// const INITIAL_STATE = {
-//   email: '',
-//   password: '',
-// };
+const INITIAL_STATE = {
+  email: '',
+  password: 0,
+  isDisable: true,
+};
 
 class Login extends React.Component {
-  // constructor(props) {
-  //   super(props);
+  constructor(props) {
+    super(props);
 
-  //   this.state = INITIAL_STATE;
+    this.state = INITIAL_STATE;
 
-  //   this.handleChange = this.handleChange.bind(this);
-  // }
+    this.handleChange = this.handleChange.bind(this);
+    this.validateInput = this.validateInput.bind(this);
+  }
 
-  // handleChange({ target }) {
-  //   const { name, value } = target;
-  //   this.setState({
-  //     [name]: value,
-  //   });
-  // }
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.validateInput());
+  }
+
+  validateInput() {
+    const { password, email } = this.state;
+    const validateEmail = /(.*)@(.*).com/;
+    const minLength = 6;
+
+    if (validateEmail.test(email) && password.length >= minLength) {
+      this.setState({
+        isDisable: false,
+      });
+      return true;
+    }
+  }
 
   render() {
-    // const { email, password } = this.state;
+    const { email, password, isDisable } = this.state;
     return (
       <div>
         <form>
@@ -31,21 +46,26 @@ class Login extends React.Component {
             type="email"
             id="email"
             name="email"
-            // value={ email }
+            value={ email }
             data-testid="email-input"
-            // onChange={ this.handleChange }
+            onChange={ this.handleChange }
           />
           <input
             type="password"
             id="password"
             name="password"
-            // value={ password }
+            value={ password }
             data-testid="password-input"
-            // onChange={ this.handleChange }
+            onChange={ this.handleChange }
           />
-          {/* <Link to="/"> */}
-          <button type="submit">Entrar</button>
-          {/* </Link> */}
+          <Link to="/carteira">
+            <button
+              type="submit"
+              disabled={ isDisable }
+            >
+              Entrar
+            </button>
+          </Link>
         </form>
       </div>
     );
