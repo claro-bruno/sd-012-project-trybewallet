c7c5f6619b4fe2f0928540fcfdb737c920e19905
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-08-06 12:17:06 -0300

    Add requirement 01 - completed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Input from '../components/Input';
+
+const MININUM_SIZE_PASSWORD = 5;
 
 class Login extends React.Component {
   constructor(props) {
@@ -7,42 +10,55 @@ class Login extends React.Component {
     this.state = {
       email: '',
       password: '',
+      isButtonDisabled: true,
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
   }
 
   handleChange({ target }) {
     const { name, value } = target;
     this.setState({ [name]: value });
+    this.validateEmail();
+  }
+
+  validateEmail() {
+    const { email, password } = this.state;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailRegex.test(email);
+    const isValidPassword = password.length >= MININUM_SIZE_PASSWORD;
+    const areUserInfosValid = isValidEmail && isValidPassword;
+
+    if (areUserInfosValid) {
+      this.setState({ isButtonDisabled: false });
+    }
   }
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, isButtonDisabled } = this.state;
 
     return (
       <>
-        <label htmlFor="email">
-          Email:
-          <input
-            name="email"
-            type="email"
-            data-testid="email-input"
-            value={ email }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="password">
-          Senha:
-          <input
-            name="password"
-            type="password"
-            data-testid="password-input"
-            value={ password }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <button type="button">
+        <Input
+          name="email"
+          label="Email: "
+          type="email"
+          testId="email-input"
+          value={ email }
+          onChange={ this.handleChange }
+          required
+        />
+        <Input
+          name="password"
+          label="Senha: "
+          type="password"
+          testId="password-input"
+          value={ password }
+          onChange={ this.handleChange }
+          required
+        />
+        <button type="button" disabled={ isButtonDisabled }>
           Entrar
         </button>
       </>
