b85d7e65a5d2b0cf1f3f21cf081c7b0c1bb951a2
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-08-06 10:34:29 -0300

    Add requirement 01

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,12 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case '':
+    return '';
+  default:
+    return state;
+  }
+};
+
+export default user;
