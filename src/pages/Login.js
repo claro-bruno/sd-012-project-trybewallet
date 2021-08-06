4627ef3e35c3b2c2af0ab84836f35bd7d143b3bc
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-08-06 10:08:39 -0300

    Add requirement 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,52 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    return (
+      <>
+        <label htmlFor="email">
+          Email:
+          <input
+            name="email"
+            type="email"
+            data-testid="email-input"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input
+            name="password"
+            type="password"
+            data-testid="password-input"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button type="button">
+          Entrar
+        </button>
+      </>
+    );
   }
 }
 
