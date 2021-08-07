e3842868a7bad2e9dceb988c88ad7540e25f7c7c
Victor <juanvictor70@gmail.com>
2021-08-07 18:45:21 -0400

    consertando o NewExpense e Actions/Index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,18 @@
 // Coloque aqui suas actions
-export const LOGIN_USER = 'LOGIN_USER';
-export const UPDATE_EXPENSE = 'UPDATE_EXPENSE';
+export const USER_LOGIN = 'USER_LOGIN';
 export const UPDATE_CURRENCY = 'UPDATE_CURRENCY';
+export const ADD_EXPENSE = 'ADD_EXPENSE';
+export const EDIT_EXPENSE = 'EDIT_EXPENSE';
+export const DELETE_EXPENSE = 'DELETE_EXPENSE';
+export const SET_LOADING = 'SET_LOADING';
 
 export const loginUser = (email) => ({
-  type: LOGIN_USER,
+  type: USER_LOGIN,
   payload: email,
+});
+
+export const loading = () => ({
+  type: SET_LOADING,
 });
 
 export const updateCurrency = (currency) => ({
@@ -13,7 +20,34 @@ export const updateCurrency = (currency) => ({
   payload: currency,
 });
 
+export const deleteExpense = (newCurrencies) => ({
+  type: DELETE_EXPENSE,
+  payload: newCurrencies,
+});
+
 export const updateExpense = (expense) => ({
-  type: UPDATE_EXPENSE,
+  type: ADD_EXPENSE,
   payload: expense,
 });
+
+export const editExpense = (expense) => ({
+  type: EDIT_EXPENSE,
+  payload: expense,
+});
+
+export const fetchTheExpense = (state) => (dispatch) => (
+  fetch('https://economia.awesomeapi.com.br/json/all')
+    .then((response) => response.json())
+    .then((response) => dispatch(updateExpense({ ...state, exchangeRates: response })))
+);
+
+export const fetchTheCurrency = () => (dispatch) => {
+  dispatch(loading());
+  return fetch('https://economia.awesomeapi.com.br/json/all')
+    .then((response) => response.json())
+    .then((response) => {
+      const allCurrencies = Object.keys(response);
+      const USDTdeleted = allCurrencies.filter((currency) => currency !== 'USDT');
+      dispatch(updateCurrency(USDTdeleted));
+    });
+};
--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -1,80 +1,127 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
 import InputExpense from './InputExpense';
 import SelectExpense from './SelectExpense';
+import { fetchTheExpense, fetchTheCurrency } from '../actions/index';
 
 class NewExpense extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
+    const { expenses } = this.props;
+    let id = 0;
+
+    if (expenses.length > 0) {
+      id = (expenses[expenses.length - 1].id) + 1;
+    }
     this.state = {
-      value: '',
+      id,
+      value: 0,
       description: '',
-      currencyOptions: [],
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: 'Comida',
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
-    fetch('https://economia.awesomeapi.com.br/json/all')
-      .then((response) => response.json())
-      .then((response) => {
-        const allCurrencies = Object.keys(response);
-        const USDTdeleted = allCurrencies.filter((currency) => currency !== 'USDT');
-        const currenciesObject = USDTdeleted.map((currency) => ({
-          value: currency,
-          description: currency,
-        }));
-        this.setState({
-          currencyOptions: [...currenciesObject],
-        });
-      });
+    const { getCurrencies } = this.props;
+    getCurrencies();
   }
 
   handleChange({ target: { name, value } }) {
     this.setState({ [name]: value });
   }
 
+  handleClick() {
+    const { saveExpense } = this.props;
+    const teste = { ...this.state };
+    saveExpense(teste);
+    this.setState((prevState) => ({
+      id: prevState.id + 1,
+      value: 0,
+      description: '',
+    }));
+  }
+
   render() {
-    const { value, description, currencyOptions } = this.state;
     const methodOptions = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
     const tagOptions = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
-
+    const { value, description } = this.state;
+    const { currencyOptions, loading } = this.props;
+    if (loading) { return <div>Carregando moedas</div>; }
     return (
       <section>
         <InputExpense
           text="Valor: "
+          type="number"
           name="value"
+          dataTestId="value-input"
           value={ value }
           onChange={ this.handleChange }
         />
         <InputExpense
           text="Descrição: "
+          type="text"
           name="description"
+          dataTestId="description-input"
           value={ description }
           onChange={ this.handleChange }
         />
         <SelectExpense
           text="Moeda"
           name="currency"
+          dataTestId="currency-input"
           onChange={ this.handleChange }
           options={ currencyOptions }
         />
         <SelectExpense
           text="Método de pagamento"
-          name="payment"
+          name="method"
+          dataTestId="method-input"
           onChange={ this.handleChange }
           options={ methodOptions }
         />
         <SelectExpense
           text="Tag"
           name="tag"
+          dataTestId="tag-input"
           onChange={ this.handleChange }
           options={ tagOptions }
         />
+        <button type="button" onClick={ this.handleClick }>Adicionar despesa</button>
       </section>
     );
   }
 }
 
-export default NewExpense;
+const mapDispatchToProps = (dispatch) => ({
+  saveExpense: (expense) => dispatch(fetchTheExpense(expense)),
+  getCurrencies: () => dispatch(fetchTheCurrency()),
+});
+
+const mapStateToProps = (state) => ({
+  currencyOptions: state.wallet.currencies,
+  expenses: state.wallet.expenses,
+  loading: state.wallet.loading,
+});
+
+NewExpense.propTypes = {
+  saveExpense: propTypes.func.isRequired,
+  getCurrencies: propTypes.func.isRequired,
+  currencyOptions: propTypes.arrayOf(propTypes.string).isRequired,
+  expenses: propTypes.arrayOf(propTypes.shape({
+    id: propTypes.number,
+  })).isRequired,
+  loading: propTypes.bool,
+};
+
+NewExpense.defaultProps = {
+  loading: false,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewExpense);
