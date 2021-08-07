4452bef82befd3fa6422c5b5cd5dbf0b8c008af2
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-08-07 19:36:19 -0300

    Projeto Trybe Wallet - Envio Requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <header>
+        <section>
+          <h3 data-testid="email-field">{`${email}`}</h3>
+          <h3 data-testid="total-field">0</h3>
+          <h3 data-testid="header-currency-field">BRL</h3>
+        </section>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
