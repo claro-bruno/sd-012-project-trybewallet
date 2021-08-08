fe1c2c5f25336d7663a9b9536bb55afdd2510648
Merlone <alestori7@gmail.com>
2021-08-08 15:08:28 -0300

    estilização de página de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { authAction, loginAction } from '../actions';
-import wallet from '../images/wallet.png';
+import trybe from '../images/trybe-logo.png';
 // - A rota para esta página deve ser "/"
 // - Existe um local para que o usuário insira seu email e senha
 // - Existe um botão com o texto "Entrar"
@@ -14,9 +14,20 @@ class Login extends React.Component {
     this.state = {
       email: '',
       password: '',
+      redirect: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.authenticator = this.authenticator.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { email } = this.state;
+    const { loginDispatch } = this.props;
+    loginDispatch(email);
+    this.setState({
+      redirect: true,
+    });
   }
 
   handleChange({ target }) {
@@ -34,45 +45,51 @@ class Login extends React.Component {
   }
 
   render() {
-    const { email, password } = this.state;
-    const { canAuthenticate, loginDispatch } = this.props;
+    const { email, password, redirect } = this.state;
+    const { canAuthenticate } = this.props;
+    if (redirect) return <Redirect to="/cateria" />;
     return (
       <div className="login">
         <div className="logo-div">
-          <img src={ wallet } alt="wallet" />
+          <div className="logo">
+            <img src={ trybe } alt="trybe logo" className="trybe" />
+            <h1>wallet</h1>
+          </div>
         </div>
         <form className="forms-div">
-          <label htmlFor="email-input">
+          <h2>Login</h2>
+          <label htmlFor="email-input" className="form-label">
             Email
             <input
               data-testid="email-input"
               type="email"
               name="email"
+              className="form-control"
               id="email-input"
               value={ email }
               onChange={ this.handleChange }
             />
           </label>
-          <label htmlFor="password-input">
+          <label htmlFor="password-input" className="form-label">
             Senha
             <input
               data-testid="password-input"
               type="password"
               name="password"
               id="password-input"
+              className="form-control"
               value={ password }
               onChange={ this.handleChange }
             />
           </label>
-          <Link to="/carteira">
-            <button
-              type="button"
-              onClick={ () => loginDispatch(email) }
-              disabled={ !canAuthenticate }
-            >
-              ENTRAR
-            </button>
-          </Link>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={ this.handleClick }
+            disabled={ !canAuthenticate }
+          >
+            ENTRAR
+          </button>
         </form>
       </div>
     );
