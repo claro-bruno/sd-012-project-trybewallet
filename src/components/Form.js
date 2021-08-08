59506b9efd8e26d9e5384adcc68adbd75758e84f
Augusto <augustocm2002@yahoo.com.br>
2021-08-07 23:33:04 -0300

    Requisito 06

---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+
+// class Form extends Component {
+//   render() {
+//     const {label, input, type, name} = this.props;
+//     return(
+//       <form>
+//   <input htmlFor={ name }>
+//     {label}
+//     <input type="text" name="name" />
+//   </label>
+// </form>
+
+//     )
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,11 +2,91 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Wallet extends React.Component {
+  addExpense() {
+    return (
+      <label htmlFor="Valor">
+        Valor
+        <input
+          type="text"
+          name="Valor"
+          placeholder="Acrescente o valor de sua despesa"
+        />
+      </label>
+    );
+  }
+
+  addExpDescription() {
+    return (
+      <label htmlFor="Descrição">
+        Descrição
+        <input
+          type="text"
+          name="Descrição"
+          placeholder="Adicione a descrição de sua despesa"
+        />
+      </label>
+    );
+  }
+
+  addCurrency() {
+    return (
+      <label htmlFor="Moeda">
+        Moeda
+        <select
+          id="Moeda"
+          alt="Moeda"
+        />
+      </label>
+    );
+  }
+
+  paymentMetod() {
+    return (
+      <label htmlFor="Método de pagamento">
+        Método de pagamento
+        <select
+          id="Método de pagamento"
+          alt="Método de pagamento"
+        >
+          <option value="Dinheiro">Dinheiro</option>
+          <option value="Cartão de crédito">Cartão de crédito</option>
+          <option value="Cartão de débito">Cartão de débito</option>
+        </select>
+      </label>
+    );
+  }
+
+  categorySelection() {
+    return (
+      <label htmlFor="Tag">
+        Tag
+        <select
+          id="Tag"
+          alt="Tag"
+        >
+          <option value="Alimentação">Alimentação</option>
+          <option value="Lazer">Lazer</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Transporte">Transporte</option>
+          <option value="Saúde">Saúde</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     return (
       <div>
         TrybeWallet
         <Header />
+        <form>
+          {this.addExpense()}
+          {this.addExpDescription()}
+          {this.addCurrency()}
+          {this.paymentMetod()}
+          {this.categorySelection()}
+
+        </form>
       </div>);
   }
 }
