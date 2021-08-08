ec73415fe782bf8c1685317a71fd939d0dd82fcf
Lucas Basques <lukasbasques@gmail.com>
2021-08-08 15:50:11 -0300

    Componentiza o Header do wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,48 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import AddForm from '../components/AddForm';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-    };
-  }
-
   render() {
-    const { total } = this.state;
-    const { userEmail } = this.props;
-
     return (
       <div>
-        <header>
-          <p
-            data-testid="email-field"
-          >
-            {`Email: ${userEmail}`}
-          </p>
-          <p
-            data-testid="total-field"
-          >
-            {`Despesa Total: ${total}`}
-          </p>
-          <p
-            data-testid="header-currency-field"
-          >
-            BRL
-          </p>
-        </header>
+        <Header />
         <AddForm />
       </div>
     );
   }
 }
-
-Wallet.propTypes = {
-  userEmail: PropTypes.string.isRequired,
-};
 
 const mapStateToProps = (state) => ({
   userEmail: state.user.email,
