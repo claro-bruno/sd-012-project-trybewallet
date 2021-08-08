3cf1bfbb4d791f5d8a806437d1a9cef1cb9c14d0
Victor Emmanuel <victorpassos2012@gmail.com>
2021-08-08 19:24:18 -0300

    Requisito 7 feito

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,18 @@
-// Coloque aqui suas actions
 export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const SAVE_COINS = 'SAVE_COINS';
 
 export const saveEmail = (payload) => (
   { type: SAVE_EMAIL, payload }
 );
+
+export const saveCoins = (payload) => (
+  { type: SAVE_COINS, payload }
+);
+
+export function getCoins() {
+  return (dispatch) => (
+    fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => response.json())
+      .then((json) => dispatch(saveCoins(json)))
+  );
+}
--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getCoins } from '../actions';
 
 class WalletForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currency: '',
+    };
+
+    this.getCurrency = this.getCurrency.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCurrency();
+  }
+
+  async getCurrency() {
+    const { coinsApi } = this.props;
+    await coinsApi();
+    const { wallet } = this.props;
+    const currency = Object.keys(wallet);
+    const formattedCurrency = currency.filter((item) => item !== 'USDT');
+    this.setState({
+      currency: formattedCurrency,
+    });
+  }
+
   render() {
+    const { currency } = this.state;
     return (
       <form>
         <label htmlFor="preco">
@@ -11,7 +40,8 @@ class WalletForm extends React.Component {
         <label htmlFor="moeda">
           Moeda
           <select id="moeda">
-            <option>Default</option>
+            { currency.length ? currency
+              .map((item) => <option key={ item }>{ item }</option>) : '' }
           </select>
         </label>
         <label htmlFor="pagamento">
@@ -41,4 +71,18 @@ class WalletForm extends React.Component {
   }
 }
 
-export default WalletForm;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  wallet: state.wallet.wallet,
+});
+
+WalletForm.propTypes = {
+  coinsApi: PropTypes.func.isRequired,
+  wallet: PropTypes.shape.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  coinsApi: (state) => dispatch(getCoins(state)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(WalletForm);
