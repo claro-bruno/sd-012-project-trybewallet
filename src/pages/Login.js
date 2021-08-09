31a6443f0cb179809cf59b021400d867aeac753a
AlyssonSene <alysson_silva@outlook.com>
2021-08-09 17:32:27 -0300

    Requisito 1, inputs de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <h2>Login</h2>
+          <label htmlFor="email">
+            Email:
+            <input
+              id="email"
+              data-testId="email-input"
+              name="email"
+            />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input
+              id="password"
+              data-testId="password-input"
+              name="password"
+            />
+          </label>
+          <input
+            type="button"
+            name="Entrar"
+            value="Entrar"
+          />
+        </form>
+      </div>
+    );
   }
 }
 
