6479fd40d22a1c7910afdb73a2437279a113c28d
AlyssonSene <alysson_silva@outlook.com>
2021-08-09 16:47:32 -0300

    Iniciando projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          Hello, TrybeWallet!
+        </div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
