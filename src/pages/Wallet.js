62ab2f291a529e484e6e25de74685326e7fe4324
Nalu <aluziapantoja@gmail.com>
2021-08-09 11:29:01 -0300

    Req6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,23 +2,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { number } from 'yargs';
+import FormExpenses from '../Components/FormExpenses';
 
 class Wallet extends React.Component {
   render() {
     const { email, despesas } = this.props;
     return (
-      <header>
-        <div>TrybeWallet</div>
-        <div data-testid="email-field">
-          E-mail:
-          { email }
-        </div>
-        <div data-testid="total-field">
-          Despesas totais:
-          { despesas.reduce((acc, curr) => acc + curr, 0) }
-          <div data-testid="header-currency-field">BRL</div>
-        </div>
-      </header>
+      <div>
+        <header>
+          <div>TrybeWallet</div>
+          <div data-testid="email-field">
+            E-mail:
+            { email }
+          </div>
+          <div data-testid="total-field">
+            Despesas totais:
+            { despesas.reduce((acc, curr) => acc + curr, 0) }
+            <div data-testid="header-currency-field">BRL</div>
+          </div>
+        </header>
+        <FormExpenses />
+      </div>
     );
   }
 }
