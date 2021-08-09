8c8d77d2c515a3221dc24ee67085274f6be14759
Nalu <aluziapantoja@gmail.com>
2021-08-09 10:37:42 -0300

    Req 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { number } from 'yargs';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email, despesas } = this.props;
+    return (
+      <header>
+        <div>TrybeWallet</div>
+        <div data-testid="email-field">
+          E-mail:
+          { email }
+        </div>
+        <div data-testid="total-field">
+          Despesas totais:
+          { despesas.reduce((acc, curr) => acc + curr, 0) }
+          <div data-testid="header-currency-field">BRL</div>
+        </div>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  despesas: state.wallet.expenses,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+  despesas: PropTypes.arrayOf(number).isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
