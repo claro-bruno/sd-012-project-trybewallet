e873ef30ed887715bc1c0c92318c2a131643ff04
AlyssonSene <alysson_silva@outlook.com>
2021-08-10 14:44:47 -0300

    configurando reducers user.js e wallet.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
         </div>
         <Switch>
           <Route exact path="/" component={ Login } />
+          <Route path="/carteira" component={ Wallet } />
         </Switch>
       </div>
     );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      disableButton: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  inputValidation() {
+    const { email, password } = this.state;
+    const minPassword = 6;
+    if (email || password >= minPassword) {
+      return this.setState({
+        disableButton: false,
+      });
+    }
+  }
+
   render() {
+    const { email, password, disableButton } = this.state;
     return (
       <div>
         <form>
@@ -12,6 +42,8 @@ class Login extends React.Component {
               id="email"
               data-testId="email-input"
               name="email"
+              value={ email }
+              onChange={ this.handleChange }
             />
           </label>
           <label htmlFor="password">
@@ -20,12 +52,15 @@ class Login extends React.Component {
               id="password"
               data-testId="password-input"
               name="password"
+              value={ password }
+              onChange={ this.handleChange }
             />
           </label>
           <input
             type="button"
             name="Entrar"
             value="Entrar"
+            disabled={ disableButton }
           />
         </form>
       </div>
