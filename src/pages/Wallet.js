ed2a086e8d50f563c5b3e4ae52f69194f1c1d40b
Luís Seixas <luiseixas2@gmail.com>
2021-08-11 19:21:03 -0300

    implementa o requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <h1>TrybeWallet</h1>
+      <header>
+        <h1>TrybeWallet</h1>
+        <p data-testid="email-field">
+          { email }
+        </p>
+        <p data-testid="total-field">
+          0
+        </p>
+        <p data-testid="header-currency-field">
+          BRL
+        </p>
+      </header>
     );
   }
 }
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string,
+}.isrequired;
+
+export default connect(mapStateToProps)(Wallet);
