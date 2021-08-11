b5c6e7094836ddae4851399cd97c4b51416ccff7
Luís Seixas <luiseixas2@gmail.com>
2021-08-11 17:25:53 -0300

    implementa o requisito 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>Hello, TrybeWallet!</div>
       <Switch>
         <Route to path="/" component={ Login } />
+        <Route to path="/carteira" component={ Wallet } />
       </Switch>
     </>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,76 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MIN_LENGTH_PASSWORD = 6;
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState((state) => ({
+      ...state,
+      [name]: value,
+    }));
+  }
+
+  emailIsValid() {
+    const { email } = this.state;
+    // https://ui.dev/validate-email-address-javascript/
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  checkPassword() {
+    const { password } = this.state;
+    return password.length >= MIN_LENGTH_PASSWORD;
+  }
+
+  checkDataLogin() {
+    return this.emailIsValid() && this.checkPassword();
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
-      <>
-        <div>Login</div>
-        <label htmlFor="email">
-          Email:
+      <form>
+        <label htmlFor="email-email">
           <input
             type="email"
+            value={ email }
             data-testid="email-input"
-            placeholder="Digite um email"
+            placeholder="Digite seu email"
+            name="email"
+            id="email-email"
+            onChange={ this.handleChange }
           />
         </label>
-        <label htmlFor="password">
-          Digite uma senha:
+        <label htmlFor="email-password">
           <input
             type="password"
+            value={ password }
             data-testid="password-input"
             placeholder="Digite sua senha"
+            name="password"
+            id="email-password"
+            onChange={ this.handleChange }
           />
         </label>
-        <button type="button">Entrar</button>
-      </>
+        <Link exact to="/carteira">
+          <button
+            type="button"
+            disabled={ !this.checkDataLogin() }
+          >
+            Entrar
+          </button>
+        </Link>
+
+      </form>
     );
   }
 }
